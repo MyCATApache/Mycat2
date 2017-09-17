6c6c5019e7e915ce15bc2dbf1d51acb7c8c3f449
yanjunli <allovertheword@163.com>
2017-09-17 17:31:20 +0800

    Update MycatSession.java

--- source/src/main/java/io/mycat/mycat2/MycatSession.java
+++ source/src/main/java/io/mycat/mycat2/MycatSession.java
@@ -9,8 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Stream;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
