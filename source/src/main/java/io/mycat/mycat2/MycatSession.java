18668f2c08985c45cd5cb251adbcdd0828c1aca1
yanjunli <allovertheword@163.com>
2017-09-17 20:20:52 +0800

    修复 30分钟内不再切换的逻辑 修改为需要进行切换时,在通知leader前 进行判断 循环切换时 也判断是否超过30分钟 30分钟是默认配置，可以通过配置修改

--- source/src/main/java/io/mycat/mycat2/MycatSession.java
+++ source/src/main/java/io/mycat/mycat2/MycatSession.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
