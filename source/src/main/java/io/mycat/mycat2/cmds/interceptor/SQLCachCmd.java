40bd09ed55e9f48ae4b1aaec9be91f3c8b70b3e1
yanjunli <allovertheword@163.com>
2017-09-28 11:39:45 +0800

    Update SQLCachCmd.java

--- source/src/main/java/io/mycat/mycat2/cmds/interceptor/SQLCachCmd.java
+++ source/src/main/java/io/mycat/mycat2/cmds/interceptor/SQLCachCmd.java
@@ -18,6 +18,54 @@ public class SQLCachCmd extends DefaultMySQLCommand {
 	
 	@Override
 	public boolean procssSQL(MycatSession session) throws IOException {
+		logger.debug("========================> SQLCachCmd {}", session.sqlContext.getRealSQL(0));
+
+		BufferSQLContext context = session.sqlContext;
+
+		// 检查当前是否为缓存的SQL
+		if (BufferSQLContext.SELECT_SQL == context.getSQLType()
+				&& BufferSQLContext.ANNOTATION_SQL_CACHE == context.getAnnotationType()) {
+			long cacheTime = context.getAnnotationValue(BufferSQLContext.ANNOTATION_CACHE_TIME);
+			long cacheTimeOut = context.getAnnotationValue(BufferSQLContext.ANNOTATION_ACCESS_COUNT);
+
+			// 放入缓存过期时间
+			session.getSessionAttrMap().put(SessionKeyEnum.SESSION_KEY_CACHE_TIMEOUT.getKey(), cacheTime);
+			// 放入临近的过期时间
+			session.getSessionAttrMap().put(SessionKeyEnum.SESSION_KEY_CACHE_TIMEOUT_CRITICAL.getKey(), cacheTimeOut);
+			// 放入sql语句
+			session.getSessionAttrMap().put(SessionKeyEnum.SESSION_KEY_CACHE_SQL_STR.getKey(),
+					session.sqlContext.getRealSQL(0));
+
+			if (null != session.curBackend) {
+				// 设置后端为使用中
+				session.curBackend.getSessionAttrMap().put(SessionKeyEnum.SESSION_KEY_CONN_IDLE_FLAG.getKey(), false);
+			}
+
+			SeqContextList seqcontext = (SeqContextList) session.getSessionAttrMap()
+					.get(SessionKeyEnum.SESSION_KEY_CACHE_MYCAT_CHAIN_SEQ.getKey());
+
+			if (null == seqcontext) {
+				seqcontext = new SeqContextList();
+				session.getSessionAttrMap().put(SessionKeyEnum.SESSION_KEY_CACHE_MYCAT_CHAIN_SEQ.getKey(), seqcontext);
+			}
+
+			seqcontext.clear();
+
+			seqcontext.setSession(session);
+			// // 首先添加缓存转换为sql
+			// seqcontext.addExec(CacheReadBufferToSql.INSTANCE);
+			// 然后为缓存是否存
+			seqcontext.addExec(CacheExistsCheck.INSTANCE);
+
+			try {
+				seqcontext.nextExec();
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				seqcontext.clear();
+			}
+		}
+
 		return super.procssSQL(session);
 	}
 	
