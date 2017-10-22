9af5d0a1c9c03f45fd90e5a1522fe15c984f3da4
yanjunli <allovertheword@163.com>
2017-10-22 23:27:19 +0800

    解决冲突

--- source/src/main/java/io/mycat/mycat2/sqlannotations/Blacklist.java
+++ source/src/main/java/io/mycat/mycat2/sqlannotations/Blacklist.java
@@ -1,15 +1,22 @@
 package io.mycat.mycat2.sqlannotations;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.mycat.mycat2.MycatSession;
 import io.mycat.mycat2.cmds.interceptor.SQLAnnotationChain;
+import io.mycat.mycat2.sqlparser.byteArrayInterface.dynamicAnnotation.impl.ConditionUtil;
 
 /**
  * Created by jamie on 2017/9/24.
  */
 public class Blacklist extends SQLAnnotation{
+	
+	List<SQLAnnotation> sqlAnnotations;
 	
 	private static final Logger logger = LoggerFactory.getLogger(Blacklist.class);
 
@@ -18,10 +25,20 @@ public class Blacklist extends SQLAnnotation{
 
     @Override
     public void init(Object args) {
+    	List<Map<String, String>> list = (List) args;
+    	sqlAnnotations = list.stream().map((i) -> 
+    					getActonFactory()
+    					.getActionByActionName(ConditionUtil.mappingKey(i),ConditionUtil.mappingValue(i)))
+    					.collect(Collectors.toList());
     }
 
     @Override
     public boolean apply(MycatSession context,SQLAnnotationChain chain) {
+    	for (SQLAnnotation f : sqlAnnotations) {
+    		 if (!f.apply(context,chain)) {
+    			 return false;
+    		 }
+    	}
         return true;
     }
 }
