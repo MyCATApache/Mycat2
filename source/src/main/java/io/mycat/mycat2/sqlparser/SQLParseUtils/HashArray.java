72094a554ed048cd573000fa1c574f42762155f7
ishotoli <kaiz@qq.com>
2017-11-18 12:29:23 +0800

    修改HashArray排列，使之适应最长长度为16M的SQL，主要是缩短了size字段，让size字段最长为255，如果超过这个长度的token，则根据下一个节点的位置来获取长度

--- source/src/main/java/io/mycat/mycat2/sqlparser/SQLParseUtils/HashArray.java
+++ source/src/main/java/io/mycat/mycat2/sqlparser/SQLParseUtils/HashArray.java
@@ -22,16 +22,25 @@ public class HashArray {
         pos = 0;
     }
     public void set(int type, int start, int size) {
-        hashArray[pos++] = (long)type << 32 | size << 16 | start; pos++; }
-    public void set(int type, int start, int size, long hash) { hashArray[pos++] = (long)type << 32 | size << 16 | start; hashArray[pos++] = hash; }
+        set(type, start, size, 0L); }
+    public void set(int type, int start, int size, long hash) {
+        if (size >= 0xFF) {
+            size = 0xFF;
+        }
+        hashArray[pos++] = (long)type << 32 | size << 24 | start&0xFFFFFF; hashArray[pos++] = hash; }
     public int getPos(int idx) { return (((int)hashArray[idx<<1]) & 0xFFFF); }
-    public int getSize(int idx) { return (((int)hashArray[idx<<1]&0xFFFF0000) >>> 16); }
+    public int getSize(int idx) {
+        int size = (((int)hashArray[idx<<1]&0xFF000000) >>> 24);
+        if (size >= 0xFF) {
+            size = getPos(idx+1) - getPos(idx);
+        }
+        return size; }
     public int getType(int idx) { return (int)((hashArray[idx<<1]&0xFFFFFFFF00000000L) >>> 32); }
     public void setType(int idx, int type) { hashArray[idx<<1] = (hashArray[idx<<1] & 0xFFFFFFFFL) | ((long)type << 32); }
     public long getHash(int idx) { return hashArray[(idx<<1)+1]; }
     public int getIntHash(int idx) {
         long value = hashArray[idx << 1];
-        return (int)(value >>> 16);
+        return (int)(value >>> 24);
     }
     public int getCount() {return pos>>1;}
 }
