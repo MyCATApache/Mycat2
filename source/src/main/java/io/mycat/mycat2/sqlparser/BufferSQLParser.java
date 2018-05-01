8c11a4114a6fda9ce96ab15b59da160c5c52e73d
ishotoli <kaiz@qq.com>
2018-05-01 21:17:27 +0800

    修改order by XX desc 被解释为DESCRIBE_SQL的bug

--- source/src/main/java/io/mycat/mycat2/sqlparser/BufferSQLParser.java
+++ source/src/main/java/io/mycat/mycat2/sqlparser/BufferSQLParser.java
@@ -517,6 +517,10 @@ public class BufferSQLParser {
                     }
                     break;
                 case IntTokenHash.DESC:
+                    if (context.getCurSQLType() != 0) {
+                        pos++;
+                        break;
+                    }
                 case IntTokenHash.DESCRIBE:
                     long hashValue;
                     if (((hashValue = hashArray.getHash(pos)) == TokenHash.DESC) ||
