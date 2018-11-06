c1c82260ce06bb4fe71ff0799ad209db1bc80669
cjw <jamie12221@outlook.com>
2018-11-07 00:06:10 +0800

    add New handshakePacket and its test

--- source/src/main/java/io/mycat/mysql/Capabilities.java
+++ source/src/main/java/io/mycat/mysql/Capabilities.java
@@ -102,14 +102,23 @@ public interface Capabilities {
 
     // Enable/disable multi-stmt support
     // 通知服务器客户端可以发送多条语句（由分号分隔）。如果该标志为没有被设置，多条语句执行。
-    public static final int CLIENT_MULTI_STATEMENTS = 65536;
+    public static final int CLIENT_MULTI_STATEMENTS = 1;
 
     // Enable/disable multi-results
     // 通知服务器客户端可以处理由多语句或者存储过程执行生成的多结果集。
     // 当打开CLIENT_MULTI_STATEMENTS时，这个标志自动的被打开。
-    public static final int CLIENT_MULTI_RESULTS = 131072;
+    public static final int CLIENT_MULTI_RESULTS = 1<<1;
 
-
+    /**
+     ServerCan send multiple resultsets for COM_STMT_EXECUTE.
+     Client
+     Can handle multiple resultsets for COM_STMT_EXECUTE.
+     Value
+     0x00040000
+     Requires
+     CLIENT_PROTOCOL_41
+     */
+    public static final int  CLIENT_PS_MULTI_RESULTS = 1<<2;
     /**
      Server
      Sends extra data in Initial Handshake Packet and supports the pluggable authentication protocol.
@@ -120,7 +129,7 @@ public interface Capabilities {
      Requires
      CLIENT_PROTOCOL_41
      */
-    public static final int CLIENT_PLUGIN_AUTH = 0x00080000;
+    public static final int CLIENT_PLUGIN_AUTH = 1<<3;
 
     /**
      Value
@@ -133,7 +142,7 @@ public interface Capabilities {
      Sends connection attributes in Protocol::HandshakeResponse41.
      */
 
-    public static final int CLIENT_CONNECT_ATTRS = 0x00100000;
+    public static final int CLIENT_CONNECT_ATTRS = 1<<4;
 
     /**
      Value
@@ -146,7 +155,7 @@ public interface Capabilities {
      Length of auth response data in Protocol::HandshakeResponse41 is a length-encoded integer.
      The flag was introduced in 5.6.6, but had the wrong value.
      */
-    public static final int CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = 0x00200000;
+    public static final int CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA = 1<<5;
 
     /**
      *Value
@@ -158,7 +167,7 @@ public interface Capabilities {
      * Client
      * Can handle expired passwords.
      */
-    public static final int CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = 0x00400000;
+    public static final int CLIENT_CAN_HANDLE_EXPIRED_PASSWORDS = 1<<6;
 
     /**
      * Value
@@ -170,7 +179,7 @@ public interface Capabilities {
      * Client
      * Expects the server to send sesson-state changes after a OK packet.
      */
-    public static final int CLIENT_SESSION_TRACK = 0x00800000;
+    public static final int CLIENT_SESSION_TRACK = 1<<7;
 
     /**
      Value
@@ -187,5 +196,5 @@ public interface Capabilities {
 
      Therefore, the EOF packet in the Text Resultset is replaced with an OK packet. EOF packets are deprecated as of MySQL 5.7.5.
      */
-    public static final int CLIENT_DEPRECATE_EOF = 0x01000000;
+    public static final int CLIENT_DEPRECATE_EOF = 1<<8;
 }
---
+++ source/src/test/java/io/mycat/mycat2/packet/HandshakePacketTest.java
@@ -0,0 +1,60 @@
+package io.mycat.mycat2.packet;
+
+import io.mycat.mysql.packet.NewHandshakePacket;
+import io.mycat.proxy.ProxyBuffer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ * cjw 294712221@qq.com
+ */
+public class HandshakePacketTest {
+
+    @Test
+    public void testReadHandshakePacket() {
+        NewHandshakePacket handshakePacket = new NewHandshakePacket();
+        handshakePacket.read(ofBuffer(pkt17));
+        Assert.assertEquals(0x0a,handshakePacket.protocolVersion);
+        Assert.assertEquals("8.0.12",handshakePacket.serverVersion);
+        Assert.assertEquals(13,handshakePacket.connectionId);
+        Assert.assertEquals(new String(of(0x15, 0x5f, 0x3e, 0x56, 0x1d, 0x15, 0x2b, 0x79)),handshakePacket.authPluginDataPartOne);
+        Assert.assertEquals(0xffff,handshakePacket.partOnecapabilityFlags.capabilities);
+        Assert.assertEquals(255,handshakePacket.characterSet);
+        Assert.assertEquals(0x0002,handshakePacket.statusFlags);
+        Assert.assertEquals(0xc3ff,handshakePacket.partTwoCapabilityFlags.capabilities);
+        Assert.assertEquals(21,handshakePacket.authPluginDataLen);
+        Assert.assertEquals(new String(new char[]{0,0,0,0,0,0,0,0,0,0}),handshakePacket.reserved);
+        Assert.assertEquals("^@y\b\u000F\u001CH~cLI}\u0000",handshakePacket.authPluginDataPartTwo);
+        Assert.assertEquals("mysql_native_password",handshakePacket.authPluginName);
+    }
+
+    static int[] pkt17 = {
+            0x0a, 0x38, 0x2e, 0x30, /* J....8.0 */
+            0x2e, 0x31, 0x32, 0x00, 0x0d, 0x00, 0x00, 0x00, /* .12..... */
+            0x15, 0x5f, 0x3e, 0x56, 0x1d, 0x15, 0x2b, 0x79, /* ._>V..+y */
+            0x00, 0xff, 0xff, 0xff, 0x02, 0x00, 0xff, 0xc3, /* ........ */
+            0x15, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* ........ */
+            0x00, 0x00, 0x00, 0x5e, 0x40, 0x79, 0x08, 0x0f, /* ...^@y.. */
+            0x1c, 0x48, 0x7e, 0x63, 0x4c, 0x49, 0x7d, 0x00, /* .H~cLI}. */
+            0x6d, 0x79, 0x73, 0x71, 0x6c, 0x5f, 0x6e, 0x61, /* mysql_na */
+            0x74, 0x69, 0x76, 0x65, 0x5f, 0x70, 0x61, 0x73, /* tive_pas */
+            0x73, 0x77, 0x6f, 0x72, 0x64, 0x00              /* sword. */
+    };
+
+    public static byte[] of(int... i) {
+        byte[] bytes = new byte[i.length];
+        int j = 0;
+        for (int i1 : i) {
+            bytes[j] = (byte) i1;
+            j++;
+        }
+        return bytes;
+    }
+    public static ProxyBuffer ofBuffer(int... i) {
+        ProxyBuffer proxyBuffer = new ProxyBuffer(ByteBuffer.wrap(of(i)));
+        proxyBuffer.writeIndex = i.length;
+        return proxyBuffer;
+    }
+}
