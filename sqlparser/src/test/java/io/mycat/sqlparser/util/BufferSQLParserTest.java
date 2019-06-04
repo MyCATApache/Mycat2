a3a31ddd1dd1f3bb79fd5776e971ef9024fb6dac
cjw <jamie12221@outlook.com>
2019-06-05 02:49:03 +0800

    improve static annotation
    Signed-off-by: cjw <jamie12221@outlook.com>

--- sqlparser/src/test/java/io/mycat/sqlparser/util/BufferSQLParserTest.java
+++ sqlparser/src/test/java/io/mycat/sqlparser/util/BufferSQLParserTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,12 +26,24 @@ public class BufferSQLParserTest {
    *
    */
   @Test
-  public void testAnnotationBalance() {
-    String sql = "/*! mycat:runOnMaster = 1,a='select a.* from customer',b=3*/ select a.* from customer a where a.company_id=1; ";
+  public void testAnnotation() {
+    String sql = "/*! mycat:runOnMaster = 1,a='select a.* from customer',b=a*/ select a.* from customer a where a.company_id=1; ";
     parser.parse(sql.getBytes(), context);
     assertEquals(BufferSQLContext.SELECT_SQL, context.getSQLType());
-    Map<String, Object> map = new HashMap<>();
-    Map<String, Object> map1 = context.getStaticAnnotation().toMapAndClear(map);
+    Map<String, Object> map = context.getStaticAnnotation().toMapAndClear(new HashMap<>());
+    Assert.assertEquals(map.get("runOnMaster"), 1L);
+    Assert.assertEquals(map.get("a"), "'select a.* from customer'");
+    Assert.assertEquals(map.get("b"), "a");
   }
 
+  @Test
+  public void testAnnotation2() {
+    String sql = "/* mycat:runOnMaster = 1,a='select a.* from customer',b=a*/ select a.* from customer a where a.company_id=1; ";
+    parser.parse(sql.getBytes(), context);
+    assertEquals(BufferSQLContext.SELECT_SQL, context.getSQLType());
+    Map<String, Object> map = context.getStaticAnnotation().toMapAndClear(new HashMap<>());
+    Assert.assertEquals(map.get("runOnMaster"), 1L);
+    Assert.assertEquals(map.get("a"), "'select a.* from customer'");
+    Assert.assertEquals(map.get("b"), "a");
+  }
 }
