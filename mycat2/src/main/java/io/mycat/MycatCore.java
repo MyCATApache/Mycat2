3aef4ad81c16d606eca3d695a150828e960f01b4
zwyqz <329636271@qq.com>
2019-06-16 00:49:41 +0800

    主从延迟修改

--- mycat2/src/main/java/io/mycat/MycatCore.java
+++ mycat2/src/main/java/io/mycat/MycatCore.java
@@ -16,17 +16,13 @@ package io.mycat;
 
 import io.mycat.config.ConfigEnum;
 import io.mycat.config.heartbeat.HeartbeatRootConfig;
-<<<<<<< HEAD
 import io.mycat.logTip.ReplicaTip;
-=======
 import io.mycat.proxy.monitor.ProxyDashboard;
->>>>>>> 63f087a06557178ca690aede971448230ed521df
 import io.mycat.proxy.ProxyRuntime;
 import io.mycat.proxy.callback.AsyncTaskCallBack;
 import io.mycat.proxy.monitor.MycatMonitor;
 import io.mycat.proxy.monitor.MycatMonitorCallback;
 import io.mycat.proxy.monitor.MycatMonitorLogCallback;
-<<<<<<< HEAD
 import io.mycat.proxy.reactor.MycatReactorThread;
 import io.mycat.proxy.session.MySQLSessionManager;
 import io.mycat.replica.MySQLDataSourceEx;
@@ -37,11 +33,9 @@ import io.mycat.router.MycatRouterConfig;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
-=======
 import io.mycat.router.MycatRouter;
 import io.mycat.router.MycatRouterConfig;
 import java.io.IOException;
->>>>>>> 63f087a06557178ca690aede971448230ed521df
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -103,12 +97,11 @@ public class MycatCore {
               ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
               HeartbeatRootConfig heartbeatRootConfig = ProxyRuntime.INSTANCE
                                                             .getConfig(ConfigEnum.HEARTBEAT);
-              long period = heartbeatRootConfig.getHeartbeat().getReplicaHeartbeatPeriod();
 
               long idleTimeout = heartbeatRootConfig.getHeartbeat().getIdleTimeout();
               long replicaIdleCheckPeriod = idleTimeout / 2;
               service.scheduleAtFixedRate(idleConnectCheck(), 0, replicaIdleCheckPeriod, TimeUnit.SECONDS);
-
+                long period = heartbeatRootConfig.getHeartbeat().getReplicaHeartbeatPeriod();
 //              service.scheduleAtFixedRate(new Runnable() {
 //                @Override
 //                public void run() {
