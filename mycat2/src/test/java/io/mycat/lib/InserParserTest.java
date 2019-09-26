f260fb4e6fbb016a2fe247f0d5a0cc1c76b64c4b
cjw <jamie12221@outlook.com>
2019-09-26 13:00:07 +0800

    add Insert parser test

--- mycat2/src/test/java/io/mycat/lib/InserParserTest.java
+++ mycat2/src/test/java/io/mycat/lib/InserParserTest.java
@@ -5,9 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.sql.Date;
 import java.sql.JDBCType;
-import java.util.Arrays;
 
 public class InserParserTest {
 
@@ -79,5 +77,46 @@ public class InserParserTest {
         Assert.assertArrayEquals("ssss".getBytes(),inserParser.getBytes(6));
         Assert.assertEquals("666.666",inserParser.getString(7));
         Assert.assertEquals(666.666, inserParser.getDouble(7), 0.0);
+        inserParser.next();
+        Assert.assertEquals("2",inserParser.getString(1));
+        Assert.assertEquals(null,inserParser.getString(2));
+        Assert.assertFalse(inserParser.next());
+    }
+
+    @Test
+    public void test3() throws Exception {
+        InserParser inserParser = new InserParser("/sql/travelrecord2.sql");
+        MycatRowMetaData rowMetaData = inserParser.metaData();
+        Assert.assertEquals("travelrecord", rowMetaData.getTableName(1));
+        Assert.assertEquals(7, rowMetaData.getColumnCount());
+        Assert.assertEquals(JDBCType.BIGINT, JDBCType.valueOf(rowMetaData.getColumnType(1)));
+        Assert.assertEquals(JDBCType.VARCHAR, JDBCType.valueOf(rowMetaData.getColumnType(2)));
+        Assert.assertEquals(JDBCType.DATE, JDBCType.valueOf(rowMetaData.getColumnType(3)));
+        Assert.assertEquals(JDBCType.DECIMAL, JDBCType.valueOf(rowMetaData.getColumnType(4)));
+        Assert.assertEquals(JDBCType.INTEGER, JDBCType.valueOf(rowMetaData.getColumnType(5)));
+        Assert.assertEquals(JDBCType.BLOB, JDBCType.valueOf(rowMetaData.getColumnType(6)));
+        Assert.assertEquals(JDBCType.DOUBLE, JDBCType.valueOf(rowMetaData.getColumnType(7)));
+        inserParser.next();
+        Assert.assertEquals("1",inserParser.getString(1));
+        Assert.assertEquals("1",inserParser.getString(2));
+        Assert.assertEquals("2019-09-12",inserParser.getString(3));
+        Assert.assertEquals(java.sql.Date.valueOf("2019-09-12"), inserParser.getDate(3));
+
+        Assert.assertEquals("222",inserParser.getString(4));
+        Assert.assertEquals(new BigDecimal("222"), inserParser.getBigDecimal(4));
+
+        Assert.assertEquals("9",inserParser.getString(5));
+        Assert.assertEquals(9,inserParser.getInt(5));
+        Assert.assertEquals("ssss",inserParser.getString(6));
+        Assert.assertArrayEquals("ssss".getBytes(),inserParser.getBytes(6));
+        Assert.assertEquals("666.666",inserParser.getString(7));
+        Assert.assertEquals(666.666, inserParser.getDouble(7), 0.0);
+        inserParser.next();
+        Assert.assertEquals("2",inserParser.getString(1));
+        Assert.assertEquals(null,inserParser.getString(2));
+        inserParser.next();
+        Assert.assertEquals("3",inserParser.getString(1));
+        Assert.assertEquals(null,inserParser.getString(2));
+        Assert.assertFalse(inserParser.next());
     }
 }
---
+++ mycat2/src/test/resources/sql/travelrecord2.sql
@@ -0,0 +1,11 @@
+create table `travelrecord` (
+	`id` bigint (20),
+	`user_id` varchar (400),
+	`traveldate` date ,
+	`fee` Decimal (11),
+	`days` int (11),
+	`blob` blob ,
+	`d` double
+);
+insert into `travelrecord` (`id`, `user_id`, `traveldate`, `fee`, `days`, `blob`, `d`) values('1','1','2019-09-12','222','9','ssss','666.666'),('2',NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `travelrecord` (`id`, `user_id`, `traveldate`, `fee`, `days`, `blob`, `d`) values('3',NULL,NULL,NULL,NULL,NULL,NULL);
