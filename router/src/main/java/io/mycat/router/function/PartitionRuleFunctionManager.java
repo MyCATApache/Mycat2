fb170010312150f650d6f56a1766d6d25156ef11
cjw <jamie12221@outlook.com>
2020-01-10 00:53:16 +0800

    router module add licenses

--- router/src/main/java/io/mycat/router/function/PartitionRuleFunctionManager.java
+++ router/src/main/java/io/mycat/router/function/PartitionRuleFunctionManager.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (C) <2020>  <mycat>
+ * <p>
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with this program.  If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
 package io.mycat.router.function;
 
 import io.mycat.config.SharingFuntionRootConfig;
@@ -8,19 +22,6 @@ import java.util.Map;
 
 public enum PartitionRuleFunctionManager {
     INSTANCE;
-//    private final ConcurrentHashMap<String, Factory> functions = new ConcurrentHashMap<>();
-//
-//    public void putFunction(SharingFuntionRootConfig.ShardingFuntion funtion) {
-//        ////////////////////////////////////check/////////////////////////////////////////////////
-//        Objects.requireNonNull(funtion.getName(), "name of function can not be empty");
-//        Objects.requireNonNull(funtion.getClazz(), "poolName of function can not be empty");
-//        ////////////////////////////////////check/////////////////////////////////////////////////
-//        putRuleAlgorithm(funtion);
-//    }
-
-//    public void removeFunction(String name) {
-//        functions.remove(name);
-//    }
 
     public static RuleFunction createFunction(String name, String clazz)
             throws Exception {
@@ -43,45 +44,13 @@ public enum PartitionRuleFunctionManager {
         return rootFunction;
     }
 
-//    public RuleFunction getRuleAlgorithm(String name, Map<String, String> properties, Map<String, String> ranges) {
-//        Optional<String> clazz = functions.values().stream().filter(i -> name.equals(i.funtion.getName())).findFirst().map(i -> i.getFuntion().getClazz());
-//        if (!clazz.isPresent()){
-//            throw new IllegalArgumentException("can not found function:"+name);
-//        }
-//        return getRuleAlgorithm(name,clazz.get(),properties,ranges);
-//    }
-
     public RuleFunction getRuleAlgorithm(String name, String clazz, Map<String, String> properties, Map<String, String> ranges) {
         try {
-//            Factory factory = functions.get(name);
-//            if (factory != null&&) {
-//                factory.get();
-//            }
             RuleFunction function = createFunction(name, clazz);
             function.callInit(properties == null ? Collections.emptyMap() : properties, ranges == null ? Collections.emptyMap() : ranges);
             return function;
         } catch (Exception e) {
             throw new RuntimeException(e);
-        }
-    }
-
-//    public void putRuleAlgorithm(SharingFuntionRootConfig.ShardingFuntion funtion) {
-//        functions.put(funtion.getName(), new Factory(funtion));
-//    }
-
-    private static class Factory {
-        final SharingFuntionRootConfig.ShardingFuntion funtion;
-
-        public Factory(SharingFuntionRootConfig.ShardingFuntion funtion) {
-            this.funtion = funtion;
-        }
-
-        public RuleFunction get() throws Exception {
-            return getRuleAlgorithm(funtion);
-        }
-
-        public SharingFuntionRootConfig.ShardingFuntion getFuntion() {
-            return funtion;
         }
     }
 
