908d14439d55caa506b6789f014af45f784976d5
cjw <jamie12221@outlook.com>
2020-01-10 13:33:52 +0800

    add test file

---
+++ mycat2/src/test/resources/sql/db1.sql
@@ -0,0 +1,48 @@
+
+CREATE TABLE `address` (
+  `id` int(11) NOT NULL,
+  `addressname` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `company` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `companyname` varchar(20) DEFAULT NULL,
+  `addressid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=235 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `test` (
+  `id` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `travelrecord` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `traveldate` date DEFAULT NULL,
+  `fee` decimal(10,0) DEFAULT NULL,
+  `days` int(11) DEFAULT NULL,
+  `blob` longblob DEFAULT NULL
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+insert  into `travelrecord`(`id`,`user_id`,`traveldate`,`fee`,`days`,`blob`) values (1,NULL,NULL,NULL,NULL,NULL);
+
+CREATE TABLE `travelrecord2` (
+  `id` bigint(20) NOT NULL,
+  `user_id` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `traveldate` date DEFAULT NULL,
+  `fee` decimal(10,0) DEFAULT NULL,
+  `days` int(11) DEFAULT NULL,
+  `blob` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE `travelrecord3` (
+  `id` bigint(20) NOT NULL,
+  `user_id` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `traveldate` date DEFAULT NULL,
+  `fee` decimal(10,0) DEFAULT NULL,
+  `days` int(11) DEFAULT NULL,
+  `blob` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
