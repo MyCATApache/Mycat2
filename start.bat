60d329be6f6b8839f51483dc07cfcdbcd768b18c
cjw <jamie12221@outlook.com>
2020-02-13 17:25:19 +0800

    更新版本号

---
+++ start.bat
@@ -0,0 +1,5 @@
+D:\Program Files\Java\jdk1.8.0_191\bin\
+java -Dfile.encoding=UTF-8 -DMYCAT_HOME=D:\git6\Mycat2\mycat2\src\main\resources  -jar D:\git6\Mycat2\mycat2-0.5-SNAPSHOP-20200110162059-single.jar
+
+java -Dfile.encoding=UTF-8 -DMYCAT_HOME=D:\newgit\f\mycat2\src\main\resources -jar 
+D:\newgit\f\mycat2\target\mycat2-0.5-SNAPSHOP-20200110152640-single.jar
