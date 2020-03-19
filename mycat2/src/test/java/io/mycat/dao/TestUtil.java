580967d722d780bad670cf29d97af24d97073949
cjw <jamie12221@outlook.com>
2020-03-19 12:03:44 +0800

    fix

--- mycat2/src/test/java/io/mycat/dao/TestUtil.java
+++ mycat2/src/test/java/io/mycat/dao/TestUtil.java
@@ -80,7 +80,7 @@ public class TestUtil {
     }
 
     public static String getString(ResultSet resultSet) throws SQLException {
-        return TextConvertor.dump(resultSet).replaceAll("\r","").replaceAll("\n","");
+        return TextConvertor.dumpResultSet(resultSet).replaceAll("\r","").replaceAll("\n","");
     }
 
 }
