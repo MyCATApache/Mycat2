4ef17ead05e4bdade6bdcfeb35ea31aaa1dba16d
cjw <jamie12221@outlook.com>
2020-04-03 18:05:14 +0800

    提交分片样例

--- example/src/main/java/io/mycat/example/readWriteSeparation/ReadWriteSeparationExample.java
+++ example/src/main/java/io/mycat/example/readWriteSeparation/ReadWriteSeparationExample.java
@@ -5,16 +5,15 @@ import io.mycat.MycatCore;
 import io.mycat.RootHelper;
 import lombok.SneakyThrows;
 
-import java.net.URISyntaxException;
 import java.nio.file.Paths;
 
-public class ReadWriteSeparation {
+public class ReadWriteSeparationExample {
     @SneakyThrows
     public static void main(String[] args) throws Exception {
-        String resource = Paths.get( ReadWriteSeparation.class.getResource("").toURI()).toAbsolutePath().toString();
+        String resource = Paths.get( ReadWriteSeparationExample.class.getResource("").toURI()).toAbsolutePath().toString();
         System.out.println(resource);
         System.setProperty("MYCAT_HOME", resource);
-        ConfigProvider bootConfig = RootHelper.INSTANCE.bootConfig(ReadWriteSeparation.class);
+        ConfigProvider bootConfig = RootHelper.INSTANCE.bootConfig(ReadWriteSeparationExample.class);
         MycatCore.INSTANCE.init(bootConfig);
     }
 }
