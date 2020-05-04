5c8dcfb7101dd104b2a8d916fbcc83f0111bb742
cjw <jamie12221@outlook.com>
2020-05-05 00:01:24 +0800

    支持拆分文件配置mycat2

--- config/src/main/java/io/mycat/FileConfigProvider.java
+++ config/src/main/java/io/mycat/FileConfigProvider.java
@@ -15,15 +15,20 @@
 package io.mycat;
 
 import com.alibaba.fastjson.JSONObject;
+import io.mycat.config.ClusterRootConfig;
+import io.mycat.config.DatasourceRootConfig;
+import io.mycat.config.PatternRootConfig;
+import io.mycat.config.ShardingQueryRootConfig;
 import io.mycat.util.JsonUtil;
 import io.mycat.util.YamlUtil;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import lombok.ToString;
-import lombok.extern.log4j.Log4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -32,14 +37,15 @@ import java.nio.file.StandardOpenOption;
 import java.text.MessageFormat;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BinaryOperator;
 
 
 public class FileConfigProvider implements ConfigProvider {
     volatile MycatConfig config;
     private String defaultPath;
     final AtomicInteger count = new AtomicInteger();
-   static final Logger logger = LoggerFactory.getLogger(FileConfigProvider.class);
-    private  HashMap<String, Object> globalVariables;
+    static final Logger logger = LoggerFactory.getLogger(FileConfigProvider.class);
+    private HashMap<String, Object> globalVariables;
 
     @Override
     public void init(Class rootClass, Map<String, String> config) throws Exception {
@@ -59,12 +65,39 @@ public class FileConfigProvider implements ConfigProvider {
         Path resolve = Paths.get(path).getParent().resolve("globalVariables.json");
         Map from = JsonUtil.from(new String(Files.readAllBytes(resolve)), Map.class);
         JSONObject map = (JSONObject) from.get("map");
-        this. globalVariables = new HashMap<>();
-        map.forEach((key,v)->{
+        this.globalVariables = new HashMap<>();
+        map.forEach((key, v) -> {
             JSONObject jsonObject = (JSONObject) v;
-            globalVariables.put(key,jsonObject.get("value"));
+            globalVariables.put(key, jsonObject.get("value"));
         });
 
+        //副配置
+        this.config = Files.walk(Paths.get(defaultPath).getParent()).filter(i -> {
+            Path fileName = i.getFileName();
+            return fileName.endsWith(".yml") || fileName.endsWith(".yaml");
+        }).distinct()
+                .map(i -> {
+                    try {
+                        return getMycatConfig(i.toString());
+                    } catch (Throwable e) {
+                        logger.warn("skip:" + i);
+                        return null;
+                    }
+                }).filter(i -> i != null).reduce(this.config, (main, config2) -> {
+
+                    List<ShardingQueryRootConfig.LogicSchemaConfig> logicSchemaConfigs = Optional.ofNullable(config2.getMetadata()).map(i -> i.getSchemas()).orElse(Collections.emptyList());
+                    List<PatternRootConfig> patternRootConfigs = Optional.ofNullable(config2.getInterceptors()).orElse(Collections.emptyList());
+                    List<DatasourceRootConfig.DatasourceConfig> datasourceConfigs = Optional.ofNullable(config2.getDatasource()).map(i -> i.getDatasources()).orElse(Collections.emptyList());
+                    List<ClusterRootConfig.ClusterConfig> clusterConfigs = Optional.ofNullable(config2.getCluster()).map(i -> i.getClusters()).orElse(Collections.emptyList());
+
+                    main.getMetadata().getSchemas().addAll(logicSchemaConfigs);
+                    main.getInterceptors().addAll(patternRootConfigs);
+                    main.getDatasource().getDatasources().addAll(datasourceConfigs);
+                    main.getCluster().getClusters().addAll(clusterConfigs);
+                    return main;
+                });
+        logger.warn("----------------------------------Combined configuration----------------------------------");
+        logger.info(YamlUtil.dump(this.config));
     }
 
     @Override
@@ -78,48 +111,53 @@ public class FileConfigProvider implements ConfigProvider {
             backup();
             YamlUtil.dumpToFile(defaultPath, YamlUtil.dump(changed));
             config = changed;
-        }catch (Throwable e){
-            logger.error("",e);
+        } catch (Throwable e) {
+            logger.error("", e);
         }
     }
 
     private void backup() {
         try {
-            YamlUtil.dumpBackupToFile(defaultPath,count.getAndIncrement(),YamlUtil.dump(config));
+            YamlUtil.dumpBackupToFile(defaultPath, count.getAndIncrement(), YamlUtil.dump(config));
         } catch (Exception e) {
-            logger.error("",e);
+            logger.error("", e);
         }
     }
 
 
-
     @Override
     public void fetchConfig(String url) throws Exception {
+        MycatConfig config = getMycatConfig(url);
+        this.config = config;
+    }
+
+    @SneakyThrows
+    private static MycatConfig getMycatConfig(String url) {
         Path asbPath = Paths.get(url).toAbsolutePath();
         if (!Files.exists(asbPath)) {
             throw new IllegalArgumentException(MessageFormat.format("path not found: {0}", Objects.toString(asbPath)));
         }
         Iterator<String> iterator = Files.lines(asbPath).iterator();
-        StringBuilder sqlGroups  = new StringBuilder();
-        StringBuilder full  = new StringBuilder();
-        boolean in= false;
-        while (iterator.hasNext()){
+        StringBuilder sqlGroups = new StringBuilder();
+        StringBuilder full = new StringBuilder();
+        boolean in = false;
+        while (iterator.hasNext()) {
             String next = iterator.next();
-            if (next.startsWith("#lib start")){
+            if (next.startsWith("#lib start")) {
                 sqlGroups.append(next).append('\n');
                 in = true;
-            }else if (in){
+            } else if (in) {
                 sqlGroups.append(next).append('\n');
-            }else if (next.startsWith("#lib end")){
+            } else if (next.startsWith("#lib end")) {
                 sqlGroups.append(next).append('\n');
-                in =false;
-            }else {
+                in = false;
+            } else {
                 full.append(next).append('\n');
             }
         }
         sqlGroups.append(full);
         System.out.println(sqlGroups);
-        config = YamlUtil.loadText(sqlGroups.toString(), MycatConfig.class);
+        return YamlUtil.loadText(sqlGroups.toString(), MycatConfig.class);
     }
 
 
@@ -135,23 +173,23 @@ public class FileConfigProvider implements ConfigProvider {
 
     @Override
     public synchronized void reportReplica(String replicaName, List<String> dataSourceList) {
-        try{
-        Path resolve = Paths.get(defaultPath).getParent().resolve("replica.log");
-        StringBuilder outputStreamWriter = new StringBuilder();
-        outputStreamWriter.append(ReplicaInfo.builder().replicaName(replicaName).dataSourceList(dataSourceList).build());
-        outputStreamWriter.append("\n");
-        logger.error("switch log: ",outputStreamWriter);
-        Files.write(resolve,outputStreamWriter.toString().getBytes(), StandardOpenOption.APPEND,StandardOpenOption.CREATE);
-        }catch (Throwable e){
-            logger.error("",e);
+        try {
+            Path resolve = Paths.get(defaultPath).getParent().resolve("replica.log");
+            StringBuilder outputStreamWriter = new StringBuilder();
+            outputStreamWriter.append(ReplicaInfo.builder().replicaName(replicaName).dataSourceList(dataSourceList).build());
+            outputStreamWriter.append("\n");
+            logger.error("switch log: ", outputStreamWriter);
+            Files.write(resolve, outputStreamWriter.toString().getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+        } catch (Throwable e) {
+            logger.error("", e);
         }
     }
 
     @Getter
     @Builder
     @ToString
-    static class ReplicaInfo{
-        String    replicaName;
+    static class ReplicaInfo {
+        String replicaName;
         List<String> dataSourceList;
     }
 }
