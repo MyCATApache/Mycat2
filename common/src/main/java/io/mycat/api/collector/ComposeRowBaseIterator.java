c6e34b3f5259ee8dad4e0a56a1714c62cc8b3133
cjw <karakapi@outlook.com>
2020-06-01 16:08:42 +0800

    show tables在读写分离情况下能显示表

---
+++ common/src/main/java/io/mycat/api/collector/ComposeRowBaseIterator.java
@@ -0,0 +1,165 @@
+package io.mycat.api.collector;
+
+import io.mycat.beans.mycat.MycatRowMetaData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Objects;
+
+/**
+ * 组合迭代器
+ */
+public class ComposeRowBaseIterator implements RowBaseIterator {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComposeRowBaseIterator.class);
+
+    final LinkedList<RowBaseIterator> seq;
+    private final MycatRowMetaData metaData;
+    private RowBaseIterator current;
+
+
+    public static ComposeRowBaseIterator of(RowBaseIterator... iterators) {
+        return new ComposeRowBaseIterator(new LinkedList<>(Arrays.asList(iterators)));
+    }
+
+    public ComposeRowBaseIterator(LinkedList<RowBaseIterator> seq) {
+        this.seq = seq;
+        this.current = Objects.requireNonNull(this.seq.get(0));
+        this.metaData = current.getMetaData();
+    }
+
+    @Override
+    public MycatRowMetaData getMetaData() {
+        return metaData;
+    }
+
+    @Override
+    public boolean next() {
+        boolean next = current.next();
+        if (next) {
+            return true;
+        }
+        try {
+            current.close();
+        } catch (Exception e) {
+            LOGGER.error("", e);
+        }
+        if (seq.isEmpty()) {
+            return false;
+        }
+        current = seq.removeFirst();
+        return next();
+    }
+
+    @Override
+    public void close() {
+        try {
+            if (current != null) {
+                current.close();
+            }
+        } catch (Exception e) {
+            LOGGER.error("", e);
+        }
+        while (!seq.isEmpty()) {
+            RowBaseIterator rowBaseIterator = seq.removeFirst();
+            try {
+                if (rowBaseIterator != null) {
+                    rowBaseIterator.close();
+                }
+            } catch (Exception e) {
+                LOGGER.error("", e);
+            }
+        }
+    }
+
+    @Override
+    public boolean wasNull() {
+        return current.wasNull();
+    }
+
+    @Override
+    public String getString(int columnIndex) {
+        return current.getString(columnIndex);
+    }
+
+    @Override
+    public boolean getBoolean(int columnIndex) {
+        return current.getBoolean(columnIndex);
+    }
+
+    @Override
+    public byte getByte(int columnIndex) {
+        return current.getByte(columnIndex);
+    }
+
+    @Override
+    public short getShort(int columnIndex) {
+        return current.getShort(columnIndex);
+    }
+
+    @Override
+    public int getInt(int columnIndex) {
+        return current.getInt(columnIndex);
+    }
+
+    @Override
+    public long getLong(int columnIndex) {
+        return current.getLong(columnIndex);
+    }
+
+    @Override
+    public float getFloat(int columnIndex) {
+        return current.getFloat(columnIndex);
+    }
+
+    @Override
+    public double getDouble(int columnIndex) {
+        return current.getDouble(columnIndex);
+    }
+
+    @Override
+    public byte[] getBytes(int columnIndex) {
+        return current.getBytes(columnIndex);
+    }
+
+    @Override
+    public Date getDate(int columnIndex) {
+        return current.getDate(columnIndex);
+    }
+
+    @Override
+    public Time getTime(int columnIndex) {
+        return current.getTime(columnIndex);
+    }
+
+    @Override
+    public Timestamp getTimestamp(int columnIndex) {
+        return current.getTimestamp(columnIndex);
+    }
+
+    @Override
+    public InputStream getAsciiStream(int columnIndex) {
+        return current.getAsciiStream(columnIndex);
+    }
+
+    @Override
+    public InputStream getBinaryStream(int columnIndex) {
+        return current.getBinaryStream(columnIndex);
+    }
+
+    @Override
+    public Object getObject(int columnIndex) {
+        return current.getObject(columnIndex);
+    }
+
+    @Override
+    public BigDecimal getBigDecimal(int columnIndex) {
+        return current.getBigDecimal(columnIndex);
+    }
+}
--- mycat2/src/main/java/io/mycat/sqlHandler/dql/ShowTablesSQLHandler.java
+++ mycat2/src/main/java/io/mycat/sqlHandler/dql/ShowTablesSQLHandler.java
@@ -1,11 +1,18 @@
 package io.mycat.sqlHandler.dql;
 
+import com.alibaba.fastsql.sql.SQLUtils;
+import com.alibaba.fastsql.sql.ast.SQLName;
 import com.alibaba.fastsql.sql.ast.statement.SQLShowTablesStatement;
 import io.mycat.MycatDataContext;
+import io.mycat.api.collector.ComposeRowBaseIterator;
+import io.mycat.api.collector.RowBaseIterator;
 import io.mycat.beans.mysql.InformationSchema.TABLES_TABLE_OBJECT;
 import io.mycat.beans.mysql.InformationSchemaRuntime;
+import io.mycat.datasource.jdbc.JdbcRuntime;
+import io.mycat.datasource.jdbc.datasource.DefaultConnection;
 import io.mycat.metadata.MetadataManager;
 import io.mycat.metadata.TableHandler;
+import io.mycat.replica.ReplicaSelectorRuntime;
 import io.mycat.router.ShowStatementRewriter;
 import io.mycat.sqlHandler.AbstractSQLHandler;
 import io.mycat.sqlHandler.ExecuteCode;
@@ -18,11 +25,13 @@ import org.slf4j.LoggerFactory;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Resource
 public class ShowTablesSQLHandler extends AbstractSQLHandler<SQLShowTablesStatement> {
     private static final Logger LOGGER = LoggerFactory.getLogger(ShowTablesSQLHandler.class);
+
     @Override
     protected ExecuteCode onExecute(SQLRequest<SQLShowTablesStatement> request, MycatDataContext dataContext, Response response) {
 
@@ -50,9 +59,42 @@ public class ShowTablesSQLHandler extends AbstractSQLHandler<SQLShowTablesStatem
         }
         TABLES_TABLE_OBJECT[] tables_table_objects = objects.toArray(new TABLES_TABLE_OBJECT[0]);
         InformationSchemaRuntime.INSTANCE.update(informationSchema -> informationSchema.TABLES = tables_table_objects);
-        String sql = ShowStatementRewriter.rewriteShowTables( dataContext.getDefaultSchema(),request.getAst());
+        String sql = ShowStatementRewriter.rewriteShowTables(dataContext.getDefaultSchema(), request.getAst());
         LOGGER.info(sql);
-        response.sendResultSet(MycatDBs.createClient(dataContext).query(sql), null);
-        return ExecuteCode.PERFORMED;
+        //show 语句变成select 语句
+
+        try (RowBaseIterator query = MycatDBs.createClient(dataContext).query(sql)) {
+            //schema上默认的targetName;
+            try {
+                SQLShowTablesStatement showTablesStatement = request.getAst();
+                SQLName from = showTablesStatement.getFrom();
+                String schema = SQLUtils.normalize(from == null ? dataContext.getDefaultSchema() : from.getSimpleName());
+                if (schema != null) {
+                    String defaultTargetName = Optional.ofNullable(MetadataManager.INSTANCE)
+                            .map(i -> i.getSchemaMap())
+                            .map(i -> i.get(schema))
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            //暂时定为没有配置分片表才读取默认targetName的表作为tables
+                            .filter(i -> i.logicTables() == null || i.logicTables().isEmpty())
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                            .map(i -> i.defaultTargetName())
+                            .orElse(null);
+                    if (defaultTargetName != null) {
+                        defaultTargetName = ReplicaSelectorRuntime.INSTANCE.getDatasourceNameByReplicaName(defaultTargetName, true, null);
+                        try (DefaultConnection connection = JdbcRuntime.INSTANCE.getConnection(defaultTargetName)) {
+                            RowBaseIterator rowBaseIterator = connection.executeQuery(sql);
+
+                            //safe
+                            response.sendResultSet(ComposeRowBaseIterator.of(rowBaseIterator, query), null);
+                            return ExecuteCode.PERFORMED;
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LOGGER.error("", e);
+            }
+            response.sendResultSet(query, null);
+            return ExecuteCode.PERFORMED;
+        }
     }
 }
