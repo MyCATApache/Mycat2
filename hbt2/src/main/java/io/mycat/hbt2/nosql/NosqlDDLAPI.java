e97e37b49032f3677474f172bf5c6dbd3a965c83
wangzihao <wangzihao@lechun.cc>
2020-06-09 01:26:14 +0800

    新增nosql与dsl的API接口定义.

--- hbt2/src/main/java/io/mycat/hbt2/nosql/NosqlDDLAPI.java
+++ hbt2/src/main/java/io/mycat/hbt2/nosql/NosqlDDLAPI.java
@@ -6,9 +6,19 @@ package io.mycat.hbt2.nosql;
  * 2020年6月9日 00:57:05
  */
 public interface NosqlDDLAPI {
+    /**
+     * 批量插入
+     * @param dsl 领域语音
+     * @return 插入结果
+     */
     BulkResponse<InsertResponse> insertBulk(NosqlDSL dsl);
     InsertResponse insert(NosqlDSL dsl);
 
+    /**
+     * 批量更新
+     * @param dsl 领域语音
+     * @return 更新结果
+     */
     BulkResponse<UpdateResponse> updateBulk(NosqlDSL dsl);
     UpdateResponse update(NosqlDSL dsl);
 
