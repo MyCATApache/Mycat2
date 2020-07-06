8bcc53bac10197c4294fcaba360fd8d3cfcd53f5
cjw <karakapi@outlook.com>
2020-07-06 20:19:04 +0800

    添加预处理blob测试

--- example/src/test/java/io/mycat/example/pstmt/PstmtShardingExample.java
+++ example/src/test/java/io/mycat/example/pstmt/PstmtShardingExample.java
@@ -17,6 +17,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class PstmtShardingExample {
     @SneakyThrows
@@ -45,16 +46,28 @@ public class PstmtShardingExample {
             thread.start();
             Thread.sleep(TimeUnit.SECONDS.toMillis(10));
         }
-//        try (Connection mySQLConnection = TestUtil.getPstmtMySQLConnection()) {
-//            PreparedStatement preparedStatement = mySQLConnection.prepareStatement("INSERT INTO `db1`.`travelrecord` (`id`,`user_id`) VALUES (?,?);");
-//            ThreadLocalRandom current = ThreadLocalRandom.current();
-//            for (int i = 0; i < 600; i++) {
-//                preparedStatement.setInt(1,i);
-//                preparedStatement.setInt(2,current.nextInt());
-//                preparedStatement.addBatch();
-//            }
-//            preparedStatement.executeBatch();
-//        }
+        try (Connection mySQLConnection = TestUtil.getPstmtMySQLConnection()) {
+            try(Statement statement1 = mySQLConnection.createStatement()){
+                statement1.execute("delete `db1`.`travelrecord`");
+            }
+            PreparedStatement preparedStatement = mySQLConnection.prepareStatement("INSERT INTO `db1`.`travelrecord` (`id`,`blob`) VALUES (?,?)");
+            StringBuilder sb = new StringBuilder();
+            IntStream.rangeClosed(0,8192).forEach(i->sb.append(i));
+            byte[] blob = sb.toString().getBytes();
+            for (int i = 1; i <= 600; i++) {
+                preparedStatement.setInt(1,i);
+                preparedStatement.setBytes(2,blob);
+                preparedStatement.addBatch();
+            }
+            preparedStatement.executeBatch();
+            Statement statement = mySQLConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select count(*) from db1.travelrecord");
+            long aLong = 0;
+            while (resultSet.next()){
+                aLong= resultSet.getLong(1);
+            }
+            Assert.assertEquals(600,aLong);
+        }
         try (Connection mySQLConnection = TestUtil.getPstmtMySQLConnection()) {
             try(Statement statement1 = mySQLConnection.createStatement()){
                 statement1.execute("delete `db1`.`travelrecord`");
