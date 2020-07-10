3d0996e00f8b747ad81d49074b3a686cdf4f606a
cjw <karakapi@outlook.com>
2020-07-10 21:42:39 +0800

    1.filter on join 可以下推 2.jdbc管理器能解集群名

--- datasource/src/main/java/io/mycat/datasource/jdbc/datasource/JdbcConnectionManager.java
+++ datasource/src/main/java/io/mycat/datasource/jdbc/datasource/JdbcConnectionManager.java
@@ -84,8 +84,10 @@ public class JdbcConnectionManager implements ConnectionManager {
 
     public DefaultConnection getConnection(String name, Boolean autocommit,
                                            int transactionIsolation, boolean readOnly) {
-        Objects.requireNonNull(name);
-        JdbcDataSource key = dataSourceMap.get(name);
+        JdbcDataSource key = Optional.ofNullable(dataSourceMap.get(name))
+                .orElseGet(()->{
+                  return dataSourceMap.get( ReplicaSelectorRuntime.INSTANCE.getDatasourceNameByReplicaName(name,true,null));
+                });
         if (key.counter.updateAndGet(operand -> {
             if (operand < key.getMaxCon()) {
                 return ++operand;
