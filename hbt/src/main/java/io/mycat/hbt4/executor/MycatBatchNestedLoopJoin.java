a1fa60f93b2e54f8cc193b5e7fdf24ad1456ccd8
cjw <karakapi@outlook.com>
2020-07-23 16:57:15 +0800

    add MycatBatchNestedLoopJoinExecutor

--- hbt/src/main/java/io/mycat/hbt4/executor/MycatBatchNestedLoopJoin.java
+++ hbt/src/main/java/io/mycat/hbt4/executor/MycatBatchNestedLoopJoin.java
@@ -1,24 +1,40 @@
 package io.mycat.hbt4.executor;
 
+import com.google.common.collect.ImmutableList;
 import io.mycat.hbt4.Executor;
 import io.mycat.hbt4.ExecutorImplementor;
 import io.mycat.hbt4.ExplainWriter;
 import io.mycat.hbt4.MycatRel;
+import org.apache.calcite.adapter.enumerable.EnumerableConvention;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelTraitSet;
+import org.apache.calcite.rel.RelCollationTraitDef;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.CorrelationId;
 import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.core.JoinRelType;
-import org.apache.calcite.rel.hint.RelHint;
+import org.apache.calcite.rel.metadata.RelMdCollation;
+import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rex.RexNode;
+import org.apache.calcite.util.ImmutableBitSet;
 
-import java.util.List;
 import java.util.Set;
 
 public class MycatBatchNestedLoopJoin extends Join implements MycatRel {
-    protected MycatBatchNestedLoopJoin(RelOptCluster cluster, RelTraitSet traitSet, List<RelHint> hints, RelNode left, RelNode right, RexNode condition, Set<CorrelationId> variablesSet, JoinRelType joinType) {
-        super(cluster, traitSet, hints, left, right, condition, variablesSet, joinType);
+
+    private final ImmutableBitSet requiredColumns;
+
+    protected MycatBatchNestedLoopJoin(
+            RelOptCluster cluster,
+            RelTraitSet traits,
+            RelNode left,
+            RelNode right,
+            RexNode condition,
+            Set<CorrelationId> variablesSet,
+            ImmutableBitSet requiredColumns,
+            JoinRelType joinType) {
+        super(cluster, traits, ImmutableList.of(), left, right, condition, variablesSet, joinType);
+        this.requiredColumns = requiredColumns;
     }
 
     @Override
@@ -31,8 +47,37 @@ public class MycatBatchNestedLoopJoin extends Join implements MycatRel {
         return null;
     }
 
+
     @Override
-    public Join copy(RelTraitSet traitSet, RexNode conditionExpr, RelNode left, RelNode right, JoinRelType joinType, boolean semiJoinDone) {
-        return null;
+    public MycatBatchNestedLoopJoin copy(RelTraitSet traitSet,
+                                         RexNode condition, RelNode left, RelNode right, JoinRelType joinType,
+                                         boolean semiJoinDone) {
+        return new MycatBatchNestedLoopJoin(getCluster(), traitSet,
+                left, right, condition, variablesSet, requiredColumns, joinType);
     }
+
+    public static MycatBatchNestedLoopJoin create(
+            RelNode left,
+            RelNode right,
+            RexNode condition,
+            ImmutableBitSet requiredColumns,
+            Set<CorrelationId> variablesSet,
+            JoinRelType joinType) {
+        final RelOptCluster cluster = left.getCluster();
+        final RelMetadataQuery mq = cluster.getMetadataQuery();
+        final RelTraitSet traitSet =
+                cluster.traitSetOf(EnumerableConvention.INSTANCE)
+                        .replaceIfs(RelCollationTraitDef.INSTANCE,
+                                () -> RelMdCollation.enumerableBatchNestedLoopJoin(mq, left, right, joinType));
+        return new MycatBatchNestedLoopJoin(
+                cluster,
+                traitSet,
+                left,
+                right,
+                condition,
+                variablesSet,
+                requiredColumns,
+                joinType);
+    }
+
 }
--- hbt/src/main/java/io/mycat/hbt4/executor/MycatBatchNestedLoopJoinExecutor.java
+++ hbt/src/main/java/io/mycat/hbt4/executor/MycatBatchNestedLoopJoinExecutor.java
@@ -1,4 +1,19 @@
 package io.mycat.hbt4.executor;
 
-public class MycatBatchNestedLoopJoinExecutor {
+import io.mycat.hbt4.Executor;
+import io.mycat.mpp.Row;
+import org.apache.calcite.linq4j.JoinType;
+import org.apache.calcite.linq4j.function.Predicate2;
+
+public class MycatBatchNestedLoopJoinExecutor extends AbstractBatchNestedLoopJoin {
+    public MycatBatchNestedLoopJoinExecutor(JoinType joinType,
+                                            Executor leftInput,
+                                            Executor rightInput,
+                                            int leftExecuterFieldCount,
+                                            int rightExecuterFieldCount,
+                                            Predicate2<Row, Row> lookup,
+                                            Predicate2<Row, Row> nonEqualCondition,
+                                            TempResultSetFactory tempResultSetFactory) {
+        super(joinType, leftInput, rightInput, leftExecuterFieldCount, rightExecuterFieldCount, lookup, nonEqualCondition, tempResultSetFactory);
+    }
 }
