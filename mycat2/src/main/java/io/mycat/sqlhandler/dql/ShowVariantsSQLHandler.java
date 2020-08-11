7b9815bb82f344b3ef3d8565b8b89996601c1c22
cjw <karakapi@outlook.com>
2020-08-11 16:14:06 +0800

    remove def ShowVariantsSQLHandler

--- mycat2/src/main/java/io/mycat/sqlhandler/dql/ShowVariantsSQLHandler.java
+++ mycat2/src/main/java/io/mycat/sqlhandler/dql/ShowVariantsSQLHandler.java
@@ -40,69 +40,73 @@ public class ShowVariantsSQLHandler extends AbstractSQLHandler<MySqlShowVariants
      * @param response
      * @return
      */
+//    @Override
+//    protected ExecuteCode onExecute(SQLRequest<MySqlShowVariantsStatement> request, MycatDataContext dataContext, Response response) {
+//        try {
+//            MySqlShowVariantsStatement ast = request.getAst();
+//
+//            boolean global = ast.isGlobal();
+//            boolean session = ast.isSession();
+//
+//            if (!global && !session) {
+//                session = true;//如果没有设置则为session
+//            }
+//            String sql = ShowStatementRewriter.rewriteVariables(ast, "SESSION_VARIABLES");
+//
+//            InformationSchema informationSchema = (InformationSchema) InformationSchemaRuntime.INSTANCE.get().clone();
+//            MycatDBClientMediator client = MycatDBs.createClient(dataContext, new MycatDBClientBasedConfig(MetadataManager.INSTANCE.getSchemaMap(),
+//                    Collections.singletonMap("information_schema", informationSchema),false));
+//
+//            try {
+//                //session值覆盖全局值
+//                Map<String, Object> globalMap = new HashMap<>();
+//                for (Map.Entry<String, Object> stringObjectEntry : RootHelper.INSTANCE.getConfigProvider().globalVariables().entrySet()) {
+//                    String key = fixKeyName(stringObjectEntry.getKey());
+//                    globalMap.put(key, stringObjectEntry.getValue());
+//                }
+//
+//
+//                Map<String, Object> sessionMap = new HashMap<>();
+//                for (String k : MycatDBs.VARIABLES_COLUMNNAME_SET) {
+//                    String keyName = fixKeyName(k);
+//                    Object variable = client.getVariable(k);
+//                    sessionMap.put(keyName,variable );
+//                    sessionMap.put(keyName.toLowerCase(),variable );
+//                    sessionMap.put(keyName.toUpperCase(),variable );
+//                }
+//                globalMap.putAll(sessionMap);
+//
+//                ArrayList<InformationSchema.SESSION_VARIABLES_TABLE_OBJECT> list = new ArrayList<>();
+//                for (Map.Entry<String, Object> entry : globalMap.entrySet()) {
+//                    list.add(InformationSchema
+//                            .SESSION_VARIABLES_TABLE_OBJECT
+//                            .builder()
+//                            .VARIABLE_NAME(entry.getKey())
+//                            .VARIABLE_VALUE(entry.getValue() == null ? null : Objects.toString(entry.getValue()))
+//                            .build());
+//                }
+//
+//                informationSchema.SESSION_VARIABLES = list.toArray(new InformationSchema.SESSION_VARIABLES_TABLE_OBJECT[0]);
+//
+//                RowBaseIterator query = client.query(sql);
+//
+//                response.sendResultSet(() -> query, () -> {
+//                    throw new UnsupportedOperationException();
+//                });
+//            } finally {
+//                client.close();
+//            }
+//        } catch (Throwable e) {
+//            LOGGER.error("", e);
+//            response.sendError(e);
+//        }
+//        return ExecuteCode.PERFORMED;
+//    }
     @Override
     protected ExecuteCode onExecute(SQLRequest<MySqlShowVariantsStatement> request, MycatDataContext dataContext, Response response) {
-        try {
-            MySqlShowVariantsStatement ast = request.getAst();
-
-            boolean global = ast.isGlobal();
-            boolean session = ast.isSession();
-
-            if (!global && !session) {
-                session = true;//如果没有设置则为session
-            }
-            String sql = ShowStatementRewriter.rewriteVariables(ast, "SESSION_VARIABLES");
-
-            InformationSchema informationSchema = (InformationSchema) InformationSchemaRuntime.INSTANCE.get().clone();
-            MycatDBClientMediator client = MycatDBs.createClient(dataContext, new MycatDBClientBasedConfig(MetadataManager.INSTANCE.getSchemaMap(),
-                    Collections.singletonMap("information_schema", informationSchema),false));
-
-            try {
-                //session值覆盖全局值
-                Map<String, Object> globalMap = new HashMap<>();
-                for (Map.Entry<String, Object> stringObjectEntry : RootHelper.INSTANCE.getConfigProvider().globalVariables().entrySet()) {
-                    String key = fixKeyName(stringObjectEntry.getKey());
-                    globalMap.put(key, stringObjectEntry.getValue());
-                }
-
-
-                Map<String, Object> sessionMap = new HashMap<>();
-                for (String k : MycatDBs.VARIABLES_COLUMNNAME_SET) {
-                    String keyName = fixKeyName(k);
-                    Object variable = client.getVariable(k);
-                    sessionMap.put(keyName,variable );
-                    sessionMap.put(keyName.toLowerCase(),variable );
-                    sessionMap.put(keyName.toUpperCase(),variable );
-                }
-                globalMap.putAll(sessionMap);
-
-                ArrayList<InformationSchema.SESSION_VARIABLES_TABLE_OBJECT> list = new ArrayList<>();
-                for (Map.Entry<String, Object> entry : globalMap.entrySet()) {
-                    list.add(InformationSchema
-                            .SESSION_VARIABLES_TABLE_OBJECT
-                            .builder()
-                            .VARIABLE_NAME(entry.getKey())
-                            .VARIABLE_VALUE(entry.getValue() == null ? null : Objects.toString(entry.getValue()))
-                            .build());
-                }
-
-                informationSchema.SESSION_VARIABLES = list.toArray(new InformationSchema.SESSION_VARIABLES_TABLE_OBJECT[0]);
-
-                RowBaseIterator query = client.query(sql);
-
-                response.sendResultSet(() -> query, () -> {
-                    throw new UnsupportedOperationException();
-                });
-            } finally {
-                client.close();
-            }
-        } catch (Throwable e) {
-            LOGGER.error("", e);
-            response.sendError(e);
-        }
+        response.tryBroadcast(request.getAst());
         return ExecuteCode.PERFORMED;
     }
-
     @NotNull
     private String fixKeyName(String key) {
         while (true) {
