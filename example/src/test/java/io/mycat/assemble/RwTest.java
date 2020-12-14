ac6e677d80dbbf32a42f68f126cb5f738f81cea0
jamie12221 <jamie12221@outlook.com>
2020-12-14 23:15:49 +0800

    pass testSharding

--- example/src/test/java/io/mycat/assemble/RwTest.java
+++ example/src/test/java/io/mycat/assemble/RwTest.java
@@ -3,16 +3,14 @@ package io.mycat.assemble;
 import io.mycat.config.GlobalBackEndTableInfoConfig;
 import io.mycat.config.ShardingBackEndTableInfoConfig;
 import io.mycat.config.ShardingFuntion;
+import io.mycat.hint.CreateDataSourceHint;
 import io.mycat.hint.CreateTableHint;
 import io.mycat.router.mycat1xfunction.PartitionByRangeMod;
-import io.mycat.router.mycat1xfunction.PartitionConstant;
-import org.apache.commons.io.IOUtils;
 import org.apache.groovy.util.Maps;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.util.Arrays;
@@ -79,14 +77,12 @@ public class RwTest implements MycatTest {
             execute(mycat, "create database " + db);
             execute(mycat, "use " + db);
 
-            execute(mycat,
-                    "/*+ mycat:createDatasource{\"name\":\"prototypeDs\",\"url\":\"jdbc:mysql://127.0.0.1:3306\",\"user\":\"root\",\"password\":\"123456\"} */;");
-            execute(mycat,
-                    "/*+ mycat:createDatasource{\"name\":\"dw0\",\"url\":\"jdbc:mysql://127.0.0.1:3306\",\"user\":\"root\",\"password\":\"123456\"} */;");
-            execute(mycat,
-                    "/*+ mycat:createDatasource{\"name\":\"dw1\",\"url\":\"jdbc:mysql://127.0.0.1:3307\",\"user\":\"admin\",\"password\":\"123456\"} */;");
-            execute(mycat,
-                    "/*+ mycat:createCluster{\"name\":\"c0\",\"masters\":[\"dw0\",\"dw1\"],\"replicas\":[]} */;");
+            execute(mycat, CreateDataSourceHint
+                    .create("dw0",
+                            "jdbc:mysql://127.0.0.1:3306"));
+            execute(mycat, CreateDataSourceHint
+                    .create("dw1",
+                            "jdbc:mysql://127.0.0.1:3306"));
 
             execute(prototypeMysql,"use mysql");
             execute(prototypeMysql,"DROP TABLE IF EXISTS MYCAT_SEQUENCE;");
@@ -94,8 +90,6 @@ public class RwTest implements MycatTest {
             execute(prototypeMysql,"DROP FUNCTION IF EXISTS `mycat_seq_nextval`;");
             execute(prototypeMysql,"CREATE FUNCTION `mycat_seq_nextval`(seq_name VARCHAR(64)) RETURNS VARCHAR(64) CHARSET latin1 DETERMINISTIC   BEGIN     DECLARE retval VARCHAR(64);     DECLARE val BIGINT;     DECLARE inc INT;     DECLARE seq_lock INT;     SET val = -1;     SET inc = 0;    SET seq_lock = -1;     SELECT GET_LOCK(seq_name, 15) INTO seq_lock;     IF seq_lock = 1 THEN      SELECT current_value + increment, increment INTO val, inc FROM MYCAT_SEQUENCE WHERE NAME = seq_name FOR UPDATE;      IF val != -1 THEN        UPDATE MYCAT_SEQUENCE SET current_value = val WHERE NAME = seq_name;      END IF;      SELECT RELEASE_LOCK(seq_name) INTO seq_lock;     END IF;     SELECT CONCAT(CAST((val - inc + 1) AS CHAR),',',CAST(inc AS CHAR)) INTO retval;     RETURN retval;   END");
             execute(prototypeMysql,"INSERT INTO `MYCAT_SEQUENCE` (`name`, `current_value`) VALUES ('"+db+"_"+tableName+"', '0');");
-            execute(mycat,
-                    "/*+ mycat:setSequence{\"name\":\"mysql_"+tableName+"\",\"clazz\":\"io.mycat.plug.sequence.SequenceMySQLGenerator\"} */;");
 
 
             execute(
@@ -121,6 +115,8 @@ public class RwTest implements MycatTest {
                                                     "0-1",1,
                                                     "1-300",2)).build())
             );
+            execute(mycat,
+                    "/*+ mycat:setSequence{\"name\":\""+db+"_"+tableName+"\",\"clazz\":\"io.mycat.plug.sequence.SequenceMySQLGenerator\",\"schemaName\":\"mysql\"} */;");
 
             execute(mycat, "insert into "+tableName+" (user_id,user_name) VALUES (1,'wang')");
             execute(mycat, "insert into "+tableName+"(user_id,user_name) VALUES (2,'zhang')");
@@ -128,7 +124,7 @@ public class RwTest implements MycatTest {
             long endTime = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(5);
             boolean res = false;
             while (true) {
-                res |= hasData(mycat, db, "global");
+                res |= hasData(mycat, db, tableName);
                 if (res||System.currentTimeMillis() > endTime) {
                     break;
                 }
