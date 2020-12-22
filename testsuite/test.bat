0c6ca63e4ced4ac4471728521e340d634ddcd51c
jamie12221 <jamie12221@outlook.com>
2020-12-22 22:58:27 +0800

    remove testsuite

--- testsuite/test.bat
+++
@@ -1,9 +0,0 @@
-SET curPath= %cd%
-copy /Y  %curPath%\target\*.jar  %curPath%\target\lib
-java -cp target/lib/* io.mycat.testsuite.tools.Testsuite src/main/resources/example.iq src/main/resources/example.iq
-java -cp target/lib/* io.mycat.testsuite.tools.Testsuite ^
---factory io.mycat.testsuite.tools.TestConnectionFactory ^
---command-handler io.mycat.testsuite.tools.TestCommandHandler ^
-src/main/resources/example.iq ^
-src/main/resources/example.iq
-
