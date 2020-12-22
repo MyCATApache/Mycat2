5b5c923e789f34f0633a23e4b688256daee72538
jamie12221 <jamie12221@outlook.com>
2020-12-23 00:27:54 +0800

    clean plug mod

--- plug/src/main/java/io/mycat/plug/loadBalance/BalanceRunOnRandomMaster.java
+++ plug/src/main/java/io/mycat/plug/loadBalance/BalanceRunOnRandomMaster.java
@@ -1,14 +1,14 @@
 /**
  * Copyright (C) <2019>  <chen junwen>
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify it under the terms of the
  * GNU General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License along with this program.  If
  * not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,23 +24,23 @@ import java.util.concurrent.ThreadLocalRandom;
  * BalanceRunOnRandomMaster
  */
 public enum BalanceRunOnRandomMaster implements LoadBalanceStrategy {
-  INSTANCE {
-    @Override
-    public LoadBalanceElement select(LoadBalanceInfo info, List<LoadBalanceElement> entityList) {
-      if (null == entityList || entityList.isEmpty()) {
-        return null;
-      }
-      ArrayList<LoadBalanceElement> masterList = new ArrayList<>(2);
-      for (LoadBalanceElement loadBalanceElement : entityList) {
-        if (loadBalanceElement.isMaster()) {
-          masterList.add(loadBalanceElement);
+    INSTANCE {
+        @Override
+        public LoadBalanceElement select(LoadBalanceInfo info, List<LoadBalanceElement> entityList) {
+            if (null == entityList || entityList.isEmpty()) {
+                return null;
+            }
+            ArrayList<LoadBalanceElement> masterList = new ArrayList<>(2);
+            for (LoadBalanceElement loadBalanceElement : entityList) {
+                if (loadBalanceElement.isMaster()) {
+                    masterList.add(loadBalanceElement);
+                }
+            }
+            if (masterList.isEmpty()) {
+                return null;
+            }
+            return masterList.get(ThreadLocalRandom.current().nextInt(0, masterList.size()));
         }
-      }
-      if (masterList.isEmpty()) {
-        return null;
-      }
-      return masterList.get(ThreadLocalRandom.current().nextInt(0, masterList.size()));
     }
-  }
 }
 
