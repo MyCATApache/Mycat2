c52b5239fcf3025f58a847208c4458a29e96be6e
jamie12221 <jamie12221@outlook.com>
2021-01-18 23:41:55 +0800

    add RxBuiltInMethod

---
+++ calcite/src/main/java/org/apache/calcite/util/RxBuiltInMethod.java
@@ -0,0 +1,81 @@
+package org.apache.calcite.util;
+
+import com.google.common.collect.ImmutableMap;
+import io.reactivex.Observable;
+import org.apache.calcite.linq4j.function.Function1;
+import org.apache.calcite.linq4j.function.Predicate1;
+import org.apache.calcite.linq4j.tree.Types;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Comparator;
+import java.util.List;
+
+public enum RxBuiltInMethod {
+    OBSERVABLE_SELECT(RxBuiltInMethodImpl.class,
+            "select",Observable.class, Function1.class),
+    OBSERVABLE_FILTER(RxBuiltInMethodImpl.class,
+            "filter",Observable.class, Predicate1.class ),
+    OBSERVABLE_CACL(RxBuiltInMethodImpl.class,"calc",Observable.class,Function1.class),
+    OBSERVABLE_UNION_ALL(RxBuiltInMethodImpl.class,"unionAll",Observable.class, List.class),
+    OBSERVABLE_TOP_N(RxBuiltInMethodImpl.class,"topN",Observable.class, Comparator.class,long.class,long.class),
+    OBSERVABLE_SORT(RxBuiltInMethodImpl.class,"sort",Observable.class,Comparator.class),
+    OBSERVABLE_LIMIT(RxBuiltInMethodImpl.class,"limit",Observable.class,long.class),
+    OBSERVABLE_OFFSET(RxBuiltInMethodImpl.class,"offset",Observable.class,long.class),
+    OBSERVABLE_TO_ENUMERABLE(RxBuiltInMethodImpl.class,"observableToEnumerable",Observable.class),
+    ENUMERABLE_TO_OBSERVABLE(RxBuiltInMethodImpl.class,"enumerableToObservable",Observable.class),
+    OBSERVABLE_MERGE_SORT(RxBuiltInMethodImpl.class,"mergeSort",List.class,Comparator.class,long.class,long.class),
+    OBSERVABLE_MERGE_SORT2(RxBuiltInMethodImpl.class,"mergeSort",List.class,Comparator.class),
+    OBSERVABLE_MATIERIAL(RxBuiltInMethodImpl.class,"matierial",Observable.class),
+
+    ;
+
+    public final Method method;
+    public final Constructor constructor;
+    public final Field field;
+
+    public static final ImmutableMap<Method, RxBuiltInMethod> MAP;
+
+    static {
+
+        final ImmutableMap.Builder<Method, RxBuiltInMethod> builder =
+                ImmutableMap.builder();
+        try {
+            for (RxBuiltInMethod value : RxBuiltInMethod.values()) {
+                if (value.method != null) {
+                    builder.put(value.method, value);
+                }
+            }
+        }catch (Throwable t){
+            t.printStackTrace();
+        }
+        MAP = builder.build();
+    }
+
+    RxBuiltInMethod(Method method, Constructor constructor, Field field) {
+        this.method = method;
+        this.constructor = constructor;
+        this.field = field;
+    }
+
+    /** Defines a method. */
+    RxBuiltInMethod(Class clazz, String methodName, Class... argumentTypes) {
+        this(Types.lookupMethod(clazz, methodName, argumentTypes), null, null);
+    }
+
+    /** Defines a constructor. */
+    RxBuiltInMethod(Class clazz, Class... argumentTypes) {
+        this(null, Types.lookupConstructor(clazz, argumentTypes), null);
+    }
+
+    /** Defines a field. */
+    RxBuiltInMethod(Class clazz, String fieldName, boolean dummy) {
+        this(null, null, Types.lookupField(clazz, fieldName));
+        assert dummy : "dummy value for method overloading must be true";
+    }
+
+    public String getMethodName() {
+        return method.getName();
+    }
+}
