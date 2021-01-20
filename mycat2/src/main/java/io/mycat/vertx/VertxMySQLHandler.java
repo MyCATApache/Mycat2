b9da3ef49bed1af1cbad8c23807d2d8ab7461995
wangzihaogithub <842156727@qq.com>
2021-01-20 14:26:45 +0800

    Vertx prepear 问题

--- mycat2/src/main/java/io/mycat/vertx/VertxMySQLHandler.java
+++ mycat2/src/main/java/io/mycat/vertx/VertxMySQLHandler.java
@@ -17,7 +17,9 @@ import io.mycat.beans.mysql.packet.DefaultPreparedOKPacket;
 import io.mycat.commands.MycatdbCommand;
 import io.mycat.config.MySQLServerCapabilityFlags;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.buffer.impl.BufferImpl;
 import io.vertx.core.net.NetSocket;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,6 +29,8 @@ import java.sql.JDBCType;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static io.mycat.beans.mysql.packet.AuthPacket.calcLenencLength;
 
@@ -45,7 +49,48 @@ public class VertxMySQLHandler {
         });
     }
 
+
+    @AllArgsConstructor
+    public static class PendingMessage {
+        private final int packetId;
+        private final Buffer event;
+        private final NetSocket socket;
+    }
+
+    private final AtomicBoolean handleIng = new AtomicBoolean(false);
+    private final ConcurrentLinkedQueue<PendingMessage> pendingMessages = new ConcurrentLinkedQueue<>();
+
+    public Buffer copyIfDirectBuf(Buffer event) {
+        if (event instanceof BufferImpl && ((BufferImpl) event).byteBuf().isDirect()) {
+            Buffer buffer = Buffer.buffer(event.length());
+            buffer.appendBuffer(event);
+            return buffer;
+        } else {
+            return event;
+        }
+    }
+
     public void handle(int packetId, Buffer event, NetSocket socket) {
+        if (handleIng.compareAndSet(false, true)) {
+            PendingMessage pendingMessage;
+            try {
+                handle0(packetId, event, socket);
+                while ((pendingMessage = pendingMessages.poll()) != null) {
+                    handle0(pendingMessage.packetId, pendingMessage.event, pendingMessage.socket);
+                }
+            } finally {
+                handleIng.set(false);
+                // check if handle set handleIng gap
+                while ((pendingMessage = pendingMessages.poll()) != null) {
+                    handle0(pendingMessage.packetId, pendingMessage.event, pendingMessage.socket);
+                }
+            }
+        } else {
+            pendingMessages.offer(new PendingMessage(packetId, copyIfDirectBuf(event), socket));
+        }
+    }
+
+    public void handle0(int packetId, Buffer event, NetSocket socket) {
         session.setPacketId(packetId);
         ReadView readView = new ReadView(event);
         try {
