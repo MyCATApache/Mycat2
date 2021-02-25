6ece9c1874eb1b9b2a89d100766f9538da639626
jamie <jamie12221@outlook.com>
2021-02-25 16:44:13 +0800

    /*+ mycat:loaddata{ 	"fileName":"xxxxx", 	"schemaName":"db1", 	"tableName":"travelrecord" } */;
    1	\N	\N	\N	\N	\N
    2	\N	\N	\N	\N	\N
    3	\N	\N	\N	\N	\N
    
     /*+ mycat:loaddata{
    	"fileName":"xxxx",
    	"delimiter":",",
    	"schemaName":"db1",
    	"tableName":"travelrecord"
    } */;
    
    1,\N,\N,\N,\N,\N
    2,\N,\N,\N,\N,\N
    3,\N,\N,\N,\N,\N
    
    pass test

--- example/src/test/java/io/mycat/sql/LoaddataTest.java
+++ example/src/test/java/io/mycat/sql/LoaddataTest.java
@@ -74,4 +74,56 @@ public class LoaddataTest implements MycatTest {
         Assert.assertEquals(3, maps.size());
 
     }
+
+    @Test
+    public void testLoaddata2() throws Exception {
+        Connection mycatConnection = getMySQLConnection(DB_MYCAT);
+        execute(mycatConnection, RESET_CONFIG);
+
+        execute(mycatConnection, "DROP DATABASE db1");
+
+
+        execute(mycatConnection, "CREATE DATABASE db1");
+
+
+        execute(mycatConnection, CreateDataSourceHint
+                .create("ds0",
+                        DB1));
+
+        execute(mycatConnection,
+                CreateClusterHint.create("c0",
+                        Arrays.asList("ds0"), Collections.emptyList()));
+
+        execute(mycatConnection, "USE `db1`;");
+
+        execute(mycatConnection, "CREATE TABLE db1.`travelrecord` (\n" +
+                "  `id` bigint NOT NULL AUTO_INCREMENT,\n" +
+                "  `user_id` varchar(100) DEFAULT NULL,\n" +
+                "  `traveldate` date DEFAULT NULL,\n" +
+                "  `fee` decimal(10,0) DEFAULT NULL,\n" +
+                "  `days` int DEFAULT NULL,\n" +
+                "  `blob` longblob,\n" +
+                "  PRIMARY KEY (`id`),\n" +
+                "  KEY `id` (`id`)\n" +
+                ") ENGINE=InnoDB  DEFAULT CHARSET=utf8"
+                + " dbpartition by hash(id) tbpartition by hash(id) tbpartitions 2 dbpartitions 2;");
+        deleteData(mycatConnection,"db1","travelrecord");
+
+        CSVFormat format = CSVFormat.MYSQL.withDelimiter(',');
+        Path mycat_loaddata_example = Files.createTempFile("", "mycat_loaddata_example");
+        StringBuilder sb = new StringBuilder();
+        CSVPrinter printer = format.print(sb);
+        printer.printRecord(new Object[]{"1", null, null, null, null, null});
+        printer.printRecord(new Object[]{"2", null, null, null, null, null});
+        printer.printRecord(new Object[]{"3", null, null, null, null, null});
+        Files.write(mycat_loaddata_example, sb.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+
+
+        execute(mycatConnection, LoaddataHint.create("db1", "travelrecord",
+                mycat_loaddata_example.toString(),Collections.singletonMap("delimiter",",")));
+
+        List<Map<String, Object>> maps = executeQuery(mycatConnection, "select * from db1.travelrecord");
+        Assert.assertEquals(3, maps.size());
+
+    }
 }
