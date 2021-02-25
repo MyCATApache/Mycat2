44a01e8caf67995dbc69de499b5fe96435e9437a
jamie <jamie12221@outlook.com>
2021-02-25 16:33:10 +0800

    /*+ mycat:loaddata{ 	"fileName":"xxxxx", 	"schemaName":"db1", 	"tableName":"travelrecord" } */;
    pass test

---
+++ example/src/test/java/io/mycat/sql/LoaddataTest.java
@@ -0,0 +1,77 @@
+package io.mycat.sql;
+
+import io.mycat.assemble.MycatTest;
+import io.mycat.hint.CreateClusterHint;
+import io.mycat.hint.CreateDataSourceHint;
+import io.mycat.hint.LoaddataHint;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.sql.Connection;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@NotThreadSafe
+@net.jcip.annotations.NotThreadSafe
+public class LoaddataTest implements MycatTest {
+
+    @Test
+    public void testLoaddata() throws Exception {
+        Connection mycatConnection = getMySQLConnection(DB_MYCAT);
+        execute(mycatConnection, RESET_CONFIG);
+
+        execute(mycatConnection, "DROP DATABASE db1");
+
+
+        execute(mycatConnection, "CREATE DATABASE db1");
+
+
+        execute(mycatConnection, CreateDataSourceHint
+                .create("ds0",
+                        DB1));
+
+        execute(mycatConnection,
+                CreateClusterHint.create("c0",
+                        Arrays.asList("ds0"), Collections.emptyList()));
+
+        execute(mycatConnection, "USE `db1`;");
+
+        execute(mycatConnection, "CREATE TABLE db1.`travelrecord` (\n" +
+                "  `id` bigint NOT NULL AUTO_INCREMENT,\n" +
+                "  `user_id` varchar(100) DEFAULT NULL,\n" +
+                "  `traveldate` date DEFAULT NULL,\n" +
+                "  `fee` decimal(10,0) DEFAULT NULL,\n" +
+                "  `days` int DEFAULT NULL,\n" +
+                "  `blob` longblob,\n" +
+                "  PRIMARY KEY (`id`),\n" +
+                "  KEY `id` (`id`)\n" +
+                ") ENGINE=InnoDB  DEFAULT CHARSET=utf8"
+                + " dbpartition by hash(id) tbpartition by hash(id) tbpartitions 2 dbpartitions 2;");
+        deleteData(mycatConnection,"db1","travelrecord");
+
+        CSVFormat format = CSVFormat.MYSQL;
+        Path mycat_loaddata_example = Files.createTempFile("", "mycat_loaddata_example");
+        StringBuilder sb = new StringBuilder();
+        CSVPrinter printer = format.print(sb);
+        printer.printRecord(new Object[]{"1", null, null, null, null, null});
+        printer.printRecord(new Object[]{"2", null, null, null, null, null});
+        printer.printRecord(new Object[]{"3", null, null, null, null, null});
+        Files.write(mycat_loaddata_example, sb.toString().getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
+
+
+        execute(mycatConnection, LoaddataHint.create("db1", "travelrecord", mycat_loaddata_example.toString()));
+
+        List<Map<String, Object>> maps = executeQuery(mycatConnection, "select * from db1.travelrecord");
+        Assert.assertEquals(3, maps.size());
+
+    }
+}
