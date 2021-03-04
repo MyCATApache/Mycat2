adf2f1728cff3c35777f528314fb4258d1c92594
jamie <jamie12221@outlook.com>
2021-03-04 14:06:16 +0800

    fix test

--- example/src/test/java/io/mycat/sql/UserCaseTest.java
+++ example/src/test/java/io/mycat/sql/UserCaseTest.java
@@ -7,7 +7,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.sql.Connection;
+import java.sql.*;
+import java.sql.Date;
 import java.util.*;
 
 @NotThreadSafe
@@ -15,8 +16,8 @@ import java.util.*;
 public class UserCaseTest implements MycatTest {
 
     @Test
-    public  void case1()throws Exception{
-        try(Connection mycatConnection = getMySQLConnection(DB_MYCAT)){
+    public void case1() throws Exception {
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT)) {
             execute(mycatConnection, RESET_CONFIG);
 
             execute(mycatConnection, "DROP DATABASE db1");
@@ -51,49 +52,56 @@ public class UserCaseTest implements MycatTest {
                     "  PRIMARY KEY (`id`)\n" +
                     ") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
 
-            deleteData(mycatConnection,"db1","travelrecord2");
+            deleteData(mycatConnection, "db1", "travelrecord2");
 
-            executeQuery(mycatConnection,"SELECT t1.id,t1.name,t2.count FROM db1.user as t1\n" +
+            executeQuery(mycatConnection, "SELECT t1.id,t1.name,t2.count FROM db1.user as t1\n" +
                     "left join (select count(1) as `count`,`user_id` from travelrecord2 group by `user_id`) \n" +
                     "as t2 on `t1`.`id` = `t2`.`user_id`;");
 
-            executeQuery(mycatConnection,"SELECT t1.id,t1.name,t2.count FROM db1.user as t1\n" +
+            executeQuery(mycatConnection, "SELECT t1.id,t1.name,t2.count FROM db1.user as t1\n" +
                     "left join (select count(1) as `count`,`user_id` from travelrecord2 group by `user_id`) \n" +
                     "as `t2` on `t1`.`id` = `t2`.`user_id`;");
-            execute(mycatConnection,"use db1");
+            execute(mycatConnection, "use db1");
 
-            execute(mycatConnection,"START TRANSACTION;\n" +
+            execute(mycatConnection, "START TRANSACTION;\n" +
                     "INSERT INTO `travelrecord2` (id,`blob`, `days`, `fee`, `traveldate`, `user_id`)\n" +
                     "VALUES (1,NULL, 3, 3, timestamp('2021-02-21 12:23:42.058156'), 'tom');\n" +
                     "SELECT ROW_COUNT();\n" +
                     "COMMIT;");
-            execute(mycatConnection,"" +
+            execute(mycatConnection, "" +
                     "UPDATE  `travelrecord2` SET id = 1 where id = 1;" +
                     "SELECT ROW_COUNT();\n");
-            execute(mycatConnection,"" +
+            execute(mycatConnection, "" +
                     "UPDATE  `user` SET id = 1 where id = 1;SELECT * from `user`; " +
                     "SELECT ROW_COUNT();\n");
-            executeQuery(mycatConnection,"SELECT ROW_COUNT();");
-            deleteData(mycatConnection,"db1","travelrecord2");
-            execute(mycatConnection,"INSERT INTO `travelrecord2`(`id`,`user_id`,`traveldate`,`fee`,`days`,`blob`)\n" +
+            executeQuery(mycatConnection, "SELECT ROW_COUNT();");
+            deleteData(mycatConnection, "db1", "travelrecord2");
+            execute(mycatConnection, "INSERT INTO `travelrecord2`(`id`,`user_id`,`traveldate`,`fee`,`days`,`blob`)\n" +
                     "VALUES (1,2,timestamp('2021-02-22 18:34:05.983692'),3.5,4,NULL)");
-            List<Map<String, Object>> maps = executeQuery(mycatConnection, "SELECT * FROM travelrecord2 WHERE id = 1;");
-            Assert.assertTrue(maps.get(0).get("traveldate").toString().endsWith("983692"));//!= 05.983692000
 
-             maps = executeQuery(mycatConnection, "SELECT * FROM travelrecord2 WHERE traveldate = '2021-02-22 18:34:05.983692';");
+            Statement statement = mycatConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT traveldate FROM travelrecord2 WHERE id = 1;");
+            resultSet.next();
+            Timestamp traveldate = resultSet.getTimestamp("traveldate");
+            Assert.assertEquals(Timestamp.valueOf("2021-02-23 10:34:05.983692"), traveldate);
+            resultSet.close();
+//            = executeQuery(mycatConnection, );
+//            Assert.assertTrue(maps.get(0).get().toString().endsWith("983692"));//!= 05.983692000
+            List<Map<String, Object>> maps;
+            maps = executeQuery(mycatConnection, "SELECT * FROM travelrecord2 WHERE traveldate = '2021-02-22 18:34:05.983692';");
             Assert.assertTrue(!maps.isEmpty());
             maps = executeQuery(mycatConnection, "SELECT * FROM travelrecord2 WHERE traveldate = timestamp('2021-02-22 18:34:05.983692');");
             Assert.assertTrue(!maps.isEmpty());
             maps = executeQuery(mycatConnection, "SELECT * FROM travelrecord2 WHERE CONVERT(traveldate,date) = '2021-2-22';");
-            execute(mycatConnection,"START TRANSACTION\n" +
+            execute(mycatConnection, "START TRANSACTION\n" +
                     "INSERT INTO `travelrecord2`(`id`,`user_id`,`traveldate`,`fee`,`days`,`blob`)\n" +
                     "VALUES \n" +
                     "(6,2,timestamp('2021-02-22 18:34:05.983692'),4.5,4,NULL),\n" +
                     "(7,2,timestamp('2021-02-22 18:34:05.983692'),4.5,4,NULL),\n" +
                     "(8,2,timestamp('2021-02-22 18:34:05.983692'),4.5,4,NULL);\n" +
                     "COMMIT;");
-            deleteData(mycatConnection,"db1","travelrecord2");
-            execute(mycatConnection,"START TRANSACTION\n" +
+            deleteData(mycatConnection, "db1", "travelrecord2");
+            execute(mycatConnection, "START TRANSACTION\n" +
                     "INSERT INTO `travelrecord2`(`id`,`user_id`,`traveldate`,`fee`,`days`,`blob`)\n" +
                     "VALUES \n" +
                     "(6,2,'2021-02-22 18:34:05.983692',4.5,4,NULL),\n" +
@@ -104,8 +112,8 @@ public class UserCaseTest implements MycatTest {
     }
 
     @Test
-    public  void case2()throws Exception{
-        try(Connection mycatConnection = getMySQLConnection(DB_MYCAT)){
+    public void case2() throws Exception {
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT)) {
             execute(mycatConnection, RESET_CONFIG);
 
             execute(mycatConnection, "DROP DATABASE db1");
@@ -130,8 +138,8 @@ public class UserCaseTest implements MycatTest {
                     "\t`is_enable` tinyint(1) not null default 1,\n" +
                     "  PRIMARY KEY (`id`)\n" +
                     ") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-            deleteData(mycatConnection,"db1","user");
-            execute(mycatConnection,"insert into `user`(`id`,`name`,`is_enable`) values (1,'abc',1);");
+            deleteData(mycatConnection, "db1", "user");
+            execute(mycatConnection, "insert into `user`(`id`,`name`,`is_enable`) values (1,'abc',1);");
             List<Map<String, Object>> maps = executeQuery(mycatConnection, "SELECT * FROM `user`;");
             Assert.assertArrayEquals(new byte[]{1}, ((byte[]) maps.get(0).get("is_enable")));
         }
@@ -139,8 +147,8 @@ public class UserCaseTest implements MycatTest {
 
 
     @Test
-    public  void case3()throws Exception{
-        try(Connection mycatConnection = getMySQLConnection(DB_MYCAT)){
+    public void case3() throws Exception {
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT)) {
             execute(mycatConnection, RESET_CONFIG);
 
             execute(mycatConnection, "DROP DATABASE db1");
@@ -165,8 +173,8 @@ public class UserCaseTest implements MycatTest {
                     "\t`is_enable` bit(1) not null default 1,\n" +
                     "  PRIMARY KEY (`id`)\n" +
                     ") ENGINE=InnoDB DEFAULT CHARSET=utf8;");
-            deleteData(mycatConnection,"db1","user");
-            execute(mycatConnection,"insert into `user`(`id`,`name`,`is_enable`) values (1,'abc',1);");
+            deleteData(mycatConnection, "db1", "user");
+            execute(mycatConnection, "insert into `user`(`id`,`name`,`is_enable`) values (1,'abc',1);");
             List<Map<String, Object>> maps = executeQuery(mycatConnection, "SELECT * FROM `user`;");
             Assert.assertArrayEquals(new byte[]{1}, ((byte[]) maps.get(0).get("is_enable")));
         }
