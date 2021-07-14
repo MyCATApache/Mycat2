9f2c1f95e81136923d00340c8a8e1c68503f5300
jamie <jamie12221@outlook.com>
2021-07-14 17:29:12 +0800

    R/W support update/delete join

--- mycat2/src/main/java/io/mycat/sqlhandler/dml/UpdateSQLHandler.java
+++ mycat2/src/main/java/io/mycat/sqlhandler/dml/UpdateSQLHandler.java
@@ -17,6 +17,8 @@ package io.mycat.sqlhandler.dml;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.statement.SQLExprTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLJoinTableSource;
+import com.alibaba.druid.sql.ast.statement.SQLTableSource;
 import com.alibaba.druid.sql.dialect.mysql.ast.statement.MySqlUpdateStatement;
 import io.mycat.*;
 import io.mycat.calcite.DrdsRunnerHelper;
@@ -45,65 +47,74 @@ public class UpdateSQLHandler extends AbstractSQLHandler<MySqlUpdateStatement> {
 
     @Override
     protected Future<Void> onExecute(SQLRequest<MySqlUpdateStatement> request, MycatDataContext dataContext, Response response) {
-        return updateHandler(request.getAst(), dataContext, (SQLExprTableSource) request.getAst().getTableSource(), response);
+        return updateHandler(request.getAst(), dataContext, request.getAst().getTableSource(), response);
     }
 
     @SneakyThrows
-    public static Future<Void> updateHandler(SQLStatement sqlStatement, MycatDataContext dataContext, SQLExprTableSource tableSource, Response receiver) {
-        String schemaName = Optional.ofNullable(tableSource.getSchema() == null ? dataContext.getDefaultSchema() : tableSource.getSchema())
-                .map(i -> SQLUtils.normalize(i)).orElse(null);
-        String tableName = SQLUtils.normalize(tableSource.getTableName());
-        SchemaHandler schemaHandler;
-        MetadataManager metadataManager = MetaClusterCurrent.wrapper(MetadataManager.class);
-        Optional<NameMap<SchemaHandler>> handlerMapOptional = Optional.ofNullable(metadataManager.getSchemaMap());
-        Optional<String> targetNameOptional = Optional.ofNullable(metadataManager.getPrototype());
-        if (!handlerMapOptional.isPresent()) {
-            if (targetNameOptional.isPresent()) {
-                return receiver.proxyUpdate(Collections.singletonList(targetNameOptional.get()), Objects.toString(sqlStatement));
-            } else {
-                return receiver.sendError(new MycatException("Unable to route:" + sqlStatement));
-            }
-        } else {
-            NameMap< SchemaHandler> handlerMap = handlerMapOptional.get();
-            schemaHandler = Optional.ofNullable(handlerMap.get(schemaName))
-                    .orElseGet(() -> {
-                        if (dataContext.getDefaultSchema() == null) {
-                            throw new MycatException("unknown schema:" + schemaName);//可能schemaName有值,但是值名不是配置的名字
-                        }
-                        return handlerMap.get(dataContext.getDefaultSchema());
-                    });
-            if (schemaHandler == null) {
-                return receiver.sendError(new MycatException("Unable to route:" + sqlStatement));
-            }
-        }
-        String defaultTargetName = schemaHandler.defaultTargetName();
-        NameMap< TableHandler> tableMap = schemaHandler.logicTables();
-        TableHandler tableHandler = tableMap.get(tableName);
-        ///////////////////////////////common///////////////////////////////
-        if (tableHandler == null) {
-            return receiver.proxyUpdate(
-                    Collections.singletonList( Objects.requireNonNull(defaultTargetName,"can not route :"+sqlStatement)),
-                    sqlStatement.toString());
-        }
-        switch (tableHandler.getType()) {
-            case NORMAL:
-                HackRouter hackRouter = new HackRouter(sqlStatement, dataContext);
-                if(hackRouter.analyse()){
-                    Pair<String, String> plan = hackRouter.getPlan();
-                    return receiver.proxyUpdate(Collections.singletonList(plan.getKey()),plan.getValue());
+    public static Future<Void> updateHandler(SQLStatement sqlStatement, MycatDataContext dataContext, SQLTableSource tableSourceArg, Response receiver) {
+        if (tableSourceArg instanceof SQLExprTableSource) {
+            SQLExprTableSource tableSource = (SQLExprTableSource) tableSourceArg;
+            String schemaName = Optional.ofNullable(tableSource.getSchema() == null ? dataContext.getDefaultSchema() : tableSource.getSchema())
+                    .map(i -> SQLUtils.normalize(i)).orElse(null);
+            String tableName = SQLUtils.normalize(tableSource.getTableName());
+            SchemaHandler schemaHandler;
+            MetadataManager metadataManager = MetaClusterCurrent.wrapper(MetadataManager.class);
+            Optional<NameMap<SchemaHandler>> handlerMapOptional = Optional.ofNullable(metadataManager.getSchemaMap());
+            Optional<String> targetNameOptional = Optional.ofNullable(metadataManager.getPrototype());
+            if (!handlerMapOptional.isPresent()) {
+                if (targetNameOptional.isPresent()) {
+                    return receiver.proxyUpdate(Collections.singletonList(targetNameOptional.get()), Objects.toString(sqlStatement));
+                } else {
+                    return receiver.sendError(new MycatException("Unable to route:" + sqlStatement));
                 }
-                break;
-            default:
-                break;
+            } else {
+                NameMap<SchemaHandler> handlerMap = handlerMapOptional.get();
+                schemaHandler = Optional.ofNullable(handlerMap.get(schemaName))
+                        .orElseGet(() -> {
+                            if (dataContext.getDefaultSchema() == null) {
+                                throw new MycatException("unknown schema:" + schemaName);//可能schemaName有值,但是值名不是配置的名字
+                            }
+                            return handlerMap.get(dataContext.getDefaultSchema());
+                        });
+                if (schemaHandler == null) {
+                    return receiver.sendError(new MycatException("Unable to route:" + sqlStatement));
+                }
+            }
+            String defaultTargetName = schemaHandler.defaultTargetName();
+            NameMap<TableHandler> tableMap = schemaHandler.logicTables();
+            TableHandler tableHandler = tableMap.get(tableName);
+            ///////////////////////////////common///////////////////////////////
+            if (tableHandler == null) {
+                return receiver.proxyUpdate(
+                        Collections.singletonList(Objects.requireNonNull(defaultTargetName, "can not route :" + sqlStatement)),
+                        sqlStatement.toString());
+            }
+            switch (tableHandler.getType()) {
+                case NORMAL:
+                    HackRouter hackRouter = new HackRouter(sqlStatement, dataContext);
+                    if (hackRouter.analyse()) {
+                        Pair<String, String> plan = hackRouter.getPlan();
+                        return receiver.proxyUpdate(Collections.singletonList(plan.getKey()), plan.getValue());
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return executeUpdate(sqlStatement, dataContext, receiver, schemaName);
         }
-        return executeUpdate(sqlStatement, dataContext, receiver, schemaName);
+        HackRouter hackRouter = new HackRouter(sqlStatement, dataContext);
+        if (hackRouter.analyse()) {
+            Pair<String, String> plan = hackRouter.getPlan();
+            return receiver.proxyUpdate(Collections.singletonList(plan.getKey()), plan.getValue());
+        }
+        return receiver.sendError(new MycatException("can not route " + sqlStatement));
     }
 
     public static Future<Void> executeUpdate(SQLStatement sqlStatement, MycatDataContext dataContext, Response receiver, String schemaName) {
         DrdsSqlWithParams drdsSqlWithParams = DrdsRunnerHelper.preParse(sqlStatement, schemaName);
         Plan plan = getPlan(drdsSqlWithParams);
         PlanImplementor planImplementor = DrdsRunnerHelper.getPlanImplementor(dataContext, receiver, drdsSqlWithParams);
-        return DrdsRunnerHelper.impl(plan,planImplementor);
+        return DrdsRunnerHelper.impl(plan, planImplementor);
     }
 
     @Nullable
