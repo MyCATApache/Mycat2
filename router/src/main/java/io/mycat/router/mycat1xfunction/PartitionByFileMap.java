b4594e8d131bdb1ec4abeca00ce6e5b9eeb4bce2
jamie <jamie12221@outlook.com>
2021-07-28 16:29:49 +0800

    fix PartitionByFileMap support BigInteger

--- router/src/main/java/io/mycat/router/mycat1xfunction/PartitionByFileMap.java
+++ router/src/main/java/io/mycat/router/mycat1xfunction/PartitionByFileMap.java
@@ -100,6 +100,7 @@ public class PartitionByFileMap extends Mycat1xSingleValueRuleFunction {
                 break;
             case "BigInteger":
                 transformation = BigInteger::new;
+                break;
             case "BigDecimal":
                 transformation = BigDecimal::new;
                 break;
