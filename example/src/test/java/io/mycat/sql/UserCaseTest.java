6d8dffba90bc4e5c202dff610eeee59ed8980fd7
jamie <jamie12221@outlook.com>
2021-09-23 17:23:05 +0800

    fix test

--- example/src/test/java/io/mycat/sql/UserCaseTest.java
+++ example/src/test/java/io/mycat/sql/UserCaseTest.java
@@ -1067,17 +1067,18 @@ public class UserCaseTest implements MycatTest {
     public void case18() throws Exception {
         try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);
              Connection mysqlConnection = getMySQLConnection(DB1)) {
-            JdbcUtils.execute(mycatConnection, "use mysql");
+            execute(mycatConnection, "CREATE DATABASE db1");
+            JdbcUtils.execute(mycatConnection, "use db1");
             JdbcUtils.execute(mycatConnection,"create table if not exists char_test(c char(1));");
 
             Statement mycatStatement = mycatConnection.createStatement();
             Statement mysqlStatement = mysqlConnection.createStatement();
 
-            ResultSet mycatResultSet = mycatStatement.executeQuery("select * from mysql.char_test");
+            ResultSet mycatResultSet = mycatStatement.executeQuery("select * from db1.char_test");
             ResultSetMetaData mycatMetaData = mycatResultSet.getMetaData();
             int mycatColumnType = mycatMetaData.getColumnType(1);
 
-            ResultSet mysqlResultSet = mysqlStatement.executeQuery("select * from mysql.char_test");
+            ResultSet mysqlResultSet = mysqlStatement.executeQuery("select * from db1.char_test");
             ResultSetMetaData mysqlMetaData = mysqlResultSet.getMetaData();
             int mysqlColumnType = mysqlMetaData.getColumnType(1);
             Assert.assertEquals(mysqlColumnType,mycatColumnType);
