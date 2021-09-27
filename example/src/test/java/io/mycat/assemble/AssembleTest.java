1d553895c1f2ae8e72f442b7b0f2dcc40e1fb352
jamie <jamie12221@outlook.com>
2021-09-27 22:57:01 +0800

    fix test

--- example/src/test/java/io/mycat/assemble/AssembleTest.java
+++ example/src/test/java/io/mycat/assemble/AssembleTest.java
@@ -506,10 +506,10 @@ public class AssembleTest implements MycatTest {
     @Test
     public void testBit() throws Exception {
         try(Connection mycat = getMySQLConnection(DB_MYCAT);
-        Connection db1Connection = getMySQLConnection(DB1);){
-            execute(mycat,"CREATE DATABASE IF NOT EXISTS db1 DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;\n" +
-                    "CREATE TABLE  if not exists db1.reader ( locked BIT) ENGINE=INNODB;");
-
+            Connection db1Connection = getMySQLConnection(DB1);){
+            mycat.setAutoCommit(true);
+            execute(mycat,"CREATE DATABASE IF NOT EXISTS db1 DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci;\n" );
+            execute(mycat, "CREATE TABLE  if not exists db1.reader ( locked BIT) ENGINE=INNODB;");
             deleteData(mycat,"db1","reader");
 
             JdbcUtils.execute(mycat,"insert db1.reader (locked) VALUES (?)",Arrays.asList(true));
@@ -517,9 +517,18 @@ public class AssembleTest implements MycatTest {
             List<Map<String, Object>> mycatMaps = executeQuery(mycat, "SELECT * FROM `db1`.`reader` LIMIT 0, 1000; ");
             List<Map<String, Object>> mysqlMaps = executeQuery(db1Connection, "SELECT * FROM `db1`.`reader` LIMIT 0, 1000; ");//[{locked=true}]
 //
-            Assert.assertTrue( "[{locked=1}]".equals(mycatMaps.toString())||"[{locked=true}]".equals(mycatMaps.toString()));
+            System.out.println("mycatMaps");
+            System.out.println(mycatMaps);
+
+            System.out.println("mysqlMaps");
+            System.out.println(mysqlMaps);
+
+            if (mysqlMaps.equals(mycatMaps)){
+
+            }else {
+                Assert.assertTrue( "[{locked=1}]".equals(mycatMaps.toString())||"[{locked=true}]".equals(mycatMaps.toString()));
+            }
         }
     }
-
 
 }
