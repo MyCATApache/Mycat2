58f5533e0ed0b14c96790c7e9694aa0b9610189f
jamie <jamie12221@outlook.com>
2021-10-05 21:59:09 +0800

    add NewMycatConnection.abandon

--- common/src/main/java/io/mycat/newquery/NewMycatConnection.java
+++ common/src/main/java/io/mycat/newquery/NewMycatConnection.java
@@ -6,23 +6,37 @@ import java.util.Collections;
 import java.util.List;
 
 public interface NewMycatConnection {
-   default void query(String sql,MysqlCollector collector){
-        prepareQuery(sql, Collections.emptyList(),collector);
+    default void query(String sql, MysqlCollector collector) {
+        prepareQuery(sql, Collections.emptyList(), collector);
     }
-    default Future<RowSet>  query(String sql){
-       return query(sql,Collections.emptyList());
+
+    default Future<RowSet> query(String sql) {
+        return query(sql, Collections.emptyList());
     }
-    Future<RowSet>  query(String sql, List<Object> params);
-    void prepareQuery(String sql, List<Object> params,MysqlCollector collector);
+
+    Future<RowSet> query(String sql, List<Object> params);
+
+    void prepareQuery(String sql, List<Object> params, MysqlCollector collector);
+
     Future<SqlResult> insert(String sql, List<Object> params);
+
     Future<SqlResult> insert(String sql);
+
     Future<SqlResult> update(String sql);
+
     Future<SqlResult> update(String sql, List<Object> params);
+
     public Future<Void> close();
-    default void onSend(){
+
+    default void onSend() {
 
     }
-    default void onRev(){
+
+    default void onRev() {
+
+    }
+
+    default void abandon() {
 
     }
 }
