0bc9409f2798285e8a7202394e735bd4763e3a4f
jamie <jamie12221@outlook.com>
2021-10-16 16:09:21 +0800

    improve MetaClusterCurrent

--- common/src/main/java/io/mycat/MetaClusterCurrent.java
+++ common/src/main/java/io/mycat/MetaClusterCurrent.java
@@ -1,12 +1,12 @@
 package io.mycat;
 
-import java.util.HashMap;
+import java.util.IdentityHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class MetaClusterCurrent {
-    public static final AtomicReference<Map<Class, Object>> context = new AtomicReference<>(new HashMap<>());
+    public static final AtomicReference<Map<Class, Object>> context = new AtomicReference<>(new IdentityHashMap<>());
 
     public static <T> T wrapper(Class<T> tClass) {
         Map<Class, Object> classObjectMap = context.get();
@@ -20,5 +20,17 @@ public class MetaClusterCurrent {
 
     public static boolean exist(Class interceptorRuntimeClass) {
         return context.get().get(interceptorRuntimeClass) != null;
+    }
+    public static IdentityHashMap<Class, Object> copyContext(Class c, Object o){
+        IdentityHashMap<Class, Object> map = new IdentityHashMap<>(context.get());
+        map.put(c,o);
+        return map;
+    }
+    public static <T>   void register(Class<T> c, Object o) {
+        register(copyContext(c,o));
+    }
+
+    public static Map<Class, Object> copyContext(){
+       return new IdentityHashMap<>(context.get());
     }
 }
