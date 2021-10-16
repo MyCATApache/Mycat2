869198afacf23bc1aa2ddd757e10e52f4f7d6a7a
jamie <jamie12221@outlook.com>
2021-10-16 16:06:03 +0800

    add FileStorageManagerImpl

---
+++ mycat2/src/main/java/io/mycat/sqlhandler/config/FileStorageManagerImpl.java
@@ -0,0 +1,59 @@
+package io.mycat.sqlhandler.config;
+
+import io.mycat.config.KVObject;
+import io.mycat.util.JsonUtil;
+import lombok.SneakyThrows;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class FileStorageManagerImpl implements StorageManager {
+    private static String suffix = "json";
+    private final Path baseDirectory;
+    private final Set<Class> registerClassSet = Collections.newSetFromMap(new IdentityHashMap<>());
+
+    public FileStorageManagerImpl(Path baseDirectory) {
+        this.baseDirectory = baseDirectory;
+    }
+
+    @Override
+    public void register(Class aClass) {
+        registerClassSet.add(aClass);
+    }
+
+    @Override
+    @SneakyThrows
+    public <T extends KVObject> KV<T> get(String path, String fileNameTemplate, Class<T> aClass) {
+        Path dir = baseDirectory.resolve(path);
+        if (Files.notExists(dir)) Files.createDirectory(dir);
+        KV<T> kv = getFilekv(fileNameTemplate, aClass, dir);
+        return kv;
+    }
+
+
+    @Override
+    public Collection<Class> registerClasses() {
+        return  new HashSet<>(registerClassSet);
+    }
+
+    @Override
+    public void syncFromNet() {
+
+    }
+
+    @Override
+    public void syncToNet() {
+
+    }
+
+    private <T extends KVObject> KV<T> getFilekv(String fileNameTemplate, Class<T> aClass, Path dir) {
+        return new FileKV(fileNameTemplate, aClass, dir, suffix);
+    }
+
+    @Override
+    public void reportReplica(Map<String, List<String>> state) {
+        Path statePath = baseDirectory.resolve("state.json");
+        FileKV.writeFile(JsonUtil.toJson(state),statePath);
+    }
+}
