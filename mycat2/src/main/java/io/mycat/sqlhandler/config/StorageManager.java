5ad3c550d36a9d4011d52d5d9ee3793596ec78f1
jamie <jamie12221@outlook.com>
2021-10-16 15:35:53 +0800

    add StorageManager

---
+++ mycat2/src/main/java/io/mycat/sqlhandler/config/StorageManager.java
@@ -0,0 +1,27 @@
+package io.mycat.sqlhandler.config;
+
+import io.mycat.ReplicaReporter;
+import io.mycat.config.KVObject;
+import lombok.SneakyThrows;
+
+import java.util.Collection;
+
+public interface StorageManager extends ReplicaReporter {
+    void register(Class aClass);
+
+    @SneakyThrows
+    default public <T extends KVObject> KV<T> get(Class<T> aClass) {
+        T t = aClass.newInstance();
+        String path = t.path();
+        String fileNameTemplate = t.fileName();
+        return get(path, fileNameTemplate, aClass);
+    }
+
+    public <T extends KVObject> KV<T> get(String path, String fileNameTemplate, Class<T> aClass);
+
+    Collection<Class> registerClasses();
+
+    void syncFromNet();
+
+    void syncToNet();
+}
