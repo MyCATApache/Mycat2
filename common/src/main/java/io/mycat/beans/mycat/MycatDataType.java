24da84b99c0b91397356a2ea59f0a136b9648d24
junwen12221 <jamie12221@outlook.com>
2021-11-29 01:33:36 +0800

    add MycatField

--- common/src/main/java/io/mycat/beans/mycat/MycatDataType.java
+++ common/src/main/java/io/mycat/beans/mycat/MycatDataType.java
@@ -1,18 +1,23 @@
 package io.mycat.beans.mycat;
 
 import io.mycat.beans.mysql.MySQLFieldsType;
+import io.mycat.beans.mysql.packet.ColumnDefPacketImpl;
 import io.vertx.mysqlclient.impl.protocol.ColumnDefinition;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.data.Numeric;
 import io.vertx.sqlclient.desc.ColumnDescriptor;
 import lombok.SneakyThrows;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
-import java.sql.JDBCType;
-import java.sql.ResultSetMetaData;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.sql.*;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 import static java.sql.ResultSetMetaData.columnNoNulls;
@@ -118,42 +123,6 @@ public enum MycatDataType {
         }
     }
 
-    public Class getMysqlColumnPacket() {
-        switch (this) {
-            case BOOLEAN:
-                return Boolean.TYPE;
-            case BIT:
-            case UNSIGNED_INT:
-            case LONG:
-            case DATE:
-            case DATETIME:
-            case TIME:
-            case UNSIGNED_LONG:
-                return Long.TYPE;
-            case TINYINT:
-                return Byte.TYPE;
-            case UNSIGNED_TINYINT:
-            case SHORT:
-            case YEAR:
-                return Short.TYPE;
-            case UNSIGNED_SHORT:
-            case INT:
-                return Integer.TYPE;
-            case DOUBLE:
-                return Double.TYPE;
-            case DECIMAL:
-                return BigDecimal.class;
-            case CHAR_BINARY:
-            case BINARY:
-            case VARCHAR_BINARY:
-            case CHAR:
-            case VARCHAR:
-                return byte[].class;
-            default:
-                throw new IllegalStateException("Unexpected value: " + this);
-        }
-    }
-
     @SneakyThrows
     public static MycatField[] from(ResultSetMetaData resultSetMetaData) {
         int columnCount = resultSetMetaData.getColumnCount();
@@ -167,7 +136,7 @@ public enum MycatDataType {
             int columnType = resultSetMetaData.getColumnType(index);
             int scale = resultSetMetaData.getScale(index);
             int precision = resultSetMetaData.getPrecision(index);
-            mycatFields[i] = getMycatField(columnLabel,columnClassName, signed, nullable, columnType, scale, precision);
+            mycatFields[i] = getMycatField(columnLabel, columnClassName, signed, nullable, columnType, scale, precision);
         }
         return mycatFields;
     }
@@ -264,10 +233,10 @@ public enum MycatDataType {
                 break;
         }
         MycatField mycatField;
-        if (mycatDataType == DECIMAL){
-            mycatField = MycatField.of(name,mycatDataType, nullable, scale, precision);
-        }else {
-            mycatField=MycatField.of(name,mycatDataType, nullable);
+        if (mycatDataType == DECIMAL) {
+            mycatField = MycatField.of(name, mycatDataType, nullable, scale, precision);
+        } else {
+            mycatField = MycatField.of(name, mycatDataType, nullable);
         }
         return mycatField;
     }
@@ -279,14 +248,224 @@ public enum MycatDataType {
         for (int i = 0; i < columnCount; i++) {
             int index = i;
             ColumnDescriptor columnDefinition = resultSetMetaData.get(index);
-            String columnClassName =columnDefinition.typeName().toLowerCase();
+            String columnClassName = columnDefinition.typeName().toLowerCase();
             boolean signed = !columnClassName.contains("unsigned");
             boolean nullable =
                     !(columnDefinition instanceof ColumnDefinition) ||
                             (((ColumnDefinition) columnDefinition).flags() & MySQLFieldsType.NOT_NULL_FLAG) == 0;
             int columnType = columnDefinition.jdbcType().getVendorTypeNumber();
-            mycatFields[i] = getMycatField(columnDefinition.name(),columnClassName, signed, nullable, columnType, 0, 0);
+            mycatFields[i] = getMycatField(columnDefinition.name(), columnClassName, signed, nullable, columnType, 0, 0);
         }
         return mycatFields;
+    }
+
+    @SneakyThrows
+    public Object convert(ResultSet resultSet, MycatField[] mycatFields, int columnIndex) {
+        MycatField mycatField = mycatFields[columnIndex];
+        int jdbcResultColumnIndex = columnIndex + 1;
+        return resultSet.getObject(jdbcResultColumnIndex, mycatField.getMycatDataType().getJavaClass());
+    }
+
+    @SneakyThrows
+    public Object convert(Row resultSet, MycatField[] mycatFields, int columnIndex) {
+        MycatField mycatField = mycatFields[columnIndex];
+        return resultSet.get(mycatField.getMycatDataType().getJavaClass(), columnIndex);
+    }
+
+    @SneakyThrows
+    public Object convert(Object[] resultSet, MycatField[] mycatFields, int columnIndex) {
+        MycatField mycatField = mycatFields[columnIndex];
+        Object o = resultSet[columnIndex];
+        if (mycatField.isNullable() && o == null) return null;
+        return mycatField.getMycatDataType().fromValue(o);
+    }
+
+    @SneakyThrows
+    private Object fromValue(Object o) {
+        switch (this) {
+            case BOOLEAN:
+                if (o == Boolean.TRUE || o == Boolean.FALSE) return o;
+                if (o instanceof Number) {
+                    if (((Number) o).byteValue() > 0) {
+                        return Boolean.TRUE;
+                    } else {
+                        return Boolean.FALSE;
+                    }
+                }
+                if (o instanceof byte[]) {
+                    o = new String((byte[]) o);
+                }
+                String s = (String) o;
+                return s.startsWith("t") || s.startsWith("T");
+            case BIT:
+                if (o instanceof Boolean) {
+                    return (Boolean) o ? 1L : 0L;
+                }
+                if (o instanceof Long) {
+                    return (Long) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).longValue();
+                }
+                if (o instanceof byte[]) {
+                    return ByteBuffer.wrap((byte[]) o).getLong();
+                }
+                break;
+            case TINYINT:
+                if (o instanceof Boolean) {
+                    return (Boolean) o ? 1L : 0L;
+                }
+                if (o instanceof Byte) {
+                    return (Byte) o;
+                }
+                if (o instanceof Short) {
+                    return (Short) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).shortValue();
+                }
+                break;
+            case UNSIGNED_TINYINT:
+            case SHORT:
+                if (o instanceof Short) {
+                    return (Short) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).shortValue();
+                }
+                break;
+            case UNSIGNED_SHORT:
+            case INT:
+                if (o instanceof Integer) {
+                    return (Integer) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).intValue();
+                }
+                break;
+            case UNSIGNED_INT:
+            case LONG:
+                if (o instanceof Long) {
+                    return (Long) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).longValue();
+                }
+                break;
+            case UNSIGNED_LONG:
+                if (o instanceof BigInteger) {
+                    return (BigInteger) o;
+                }
+                if (o instanceof Long) {
+                    return BigInteger.valueOf((Long) o);
+                }
+                if (o instanceof Numeric) {
+                    return ((Numeric) o).bigIntegerValue();
+                }
+                if (o instanceof Number) {
+                    return BigInteger.valueOf(((Number) o).longValue());
+                }
+
+                break;
+            case DOUBLE:
+                if (o instanceof Double) {
+                    return (Double) o;
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).doubleValue();
+                }
+                break;
+            case DECIMAL:
+                if (o instanceof BigDecimal) {
+                    return (BigDecimal) o;
+                }
+                if (o instanceof BigInteger) {
+                    return new BigDecimal(o.toString());
+                }
+                if (o instanceof Numeric) {
+                    return ((Numeric) o).bigDecimalValue();
+                }
+                if (o instanceof Number) {
+                    return new BigDecimal(o.toString());
+                }
+                break;
+            case DATE:
+                if (o instanceof Date) {
+                    return ((Date) o).toLocalDate();
+                }
+                if (o instanceof java.util.Date) {
+                    long time = ((java.util.Date) o).getTime();
+                    return (new Date(time)).toLocalDate();
+                }
+                if (o instanceof LocalDate) {
+                    return o;
+                }
+                break;
+            case DATETIME:
+                if (o instanceof Timestamp) {
+                    return ((Timestamp) o).toLocalDateTime();
+                }
+                if (o instanceof LocalDateTime) {
+                    return o;
+                }
+                break;
+            case TIME:
+                if (o instanceof Time) {
+                    Time time = (Time) o;
+                    o = time.toLocalTime();
+                }
+                if (o instanceof LocalTime) {
+                    LocalTime time = (LocalTime) o;
+                    return Duration.ofHours(time.getHour()).plusMinutes(time.getMinute()).plusSeconds(time.getSecond());
+                }
+                break;
+            case YEAR:
+                if (o instanceof Number) {
+                    return ((Number) o).shortValue();
+                }
+                break;
+            case CHAR_BINARY:
+            case VARCHAR_BINARY:
+                if (o instanceof String) {
+                    return ((String) o).getBytes(StandardCharsets.UTF_8);
+                }
+                if (o instanceof byte[]) {
+                    return o;
+                }
+                break;
+            case VARCHAR:
+            case CHAR:
+                if (o instanceof String) {
+                    return o;
+                }
+                if (o instanceof Character) {
+                    return String.valueOf(((Character) o).charValue());
+                }
+                if (o instanceof Clob) {
+                    Clob clob = (Clob) o;
+                    return String.valueOf(clob.getSubString(1,(int)clob.length()));
+                }
+                break;
+
+            case BINARY:
+                if (o instanceof Blob) {
+                    return ((Blob) o).getBytes(1, (int)((Blob) o).length());
+                }
+                if (o instanceof byte[]) {
+                    return ((byte[]) o);
+                }
+            case FLOAT:
+                if (o instanceof Float) {
+                    return o;
+                }
+                if (o instanceof Double) {
+                    return ((Double) o).floatValue();
+                }
+                if (o instanceof Number) {
+                    return ((Number) o).floatValue();
+                }
+                break;
+        }
+        throw new UnsupportedOperationException();
     }
 }
--- common/src/main/java/io/mycat/beans/mycat/MycatField.java
+++ common/src/main/java/io/mycat/beans/mycat/MycatField.java
@@ -1,5 +1,8 @@
 package io.mycat.beans.mycat;
 
+import com.mysql.cj.CharsetMapping;
+import io.mycat.beans.mysql.MySQLFieldsType;
+import io.mycat.beans.mysql.packet.ColumnDefPacketImpl;
 import lombok.Getter;
 
 @Getter
@@ -10,31 +13,138 @@ public class MycatField {
     private final byte precision;
     private final boolean nullable;
 
-    public MycatField(String name,MycatDataType mycatDataType,boolean nullable) {
+    public MycatField(String name, MycatDataType mycatDataType, boolean nullable) {
         this.name = name;
         this.mycatDataType = mycatDataType;
         this.scale = 0;
         this.precision = 0;
         this.nullable = nullable;
     }
-    public MycatField(String name,MycatDataType mycatDataType,boolean nullable,int scale, int precision) {
+
+    public MycatField(String name, MycatDataType mycatDataType, boolean nullable, int scale, int precision) {
         this.name = name;
         this.mycatDataType = mycatDataType;
         this.scale = (byte) scale;
         this.precision = (byte) precision;
         this.nullable = nullable;
     }
-    public static MycatField of(String name,MycatDataType mycatDataType,boolean nullable){
-        return new MycatField(name,mycatDataType,nullable,0,0);
+
+    public static MycatField of(String name, MycatDataType mycatDataType, boolean nullable) {
+        return new MycatField(name, mycatDataType, nullable, 0, 0);
     }
-    public static MycatField of(String name,MycatDataType mycatDataType,boolean nullable,int scale,int precision){
-        return new MycatField(name,mycatDataType,nullable,scale,precision);
+
+    public static MycatField of(String name, MycatDataType mycatDataType, boolean nullable, int scale, int precision) {
+        return new MycatField(name, mycatDataType, nullable, scale, precision);
     }
+
     public int getScale() {
         return scale;
     }
 
     public int getPrecision() {
         return precision;
+    }
+
+   public ColumnDefPacketImpl toColumnDefPacket() {
+
+        int mysqlColumnType = 0;
+        int columnFlags = 0;
+        int columnLength = 255;
+        int columnCharsetSet = 0x21;
+        boolean decimal = false;
+        if (!nullable) {
+            columnFlags |= MySQLFieldsType.NOT_NULL_FLAG;
+        }
+
+        switch (mycatDataType) {
+            case BOOLEAN:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_BIT;
+                columnLength = 1;
+                break;
+            case BIT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_BIT;
+                break;
+            case TINYINT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_TINY;
+                break;
+            case UNSIGNED_TINYINT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_TINY;
+                columnFlags = columnFlags | MySQLFieldsType.UNSIGNED_FLAG;
+                break;
+            case SHORT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_SHORT;
+                break;
+            case UNSIGNED_SHORT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_SHORT;
+                columnFlags = columnFlags | MySQLFieldsType.UNSIGNED_FLAG;
+                break;
+            case INT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_LONG;
+                break;
+            case UNSIGNED_INT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_LONG;
+                columnFlags = columnFlags | MySQLFieldsType.UNSIGNED_FLAG;
+                break;
+            case LONG:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_LONGLONG;
+                break;
+            case UNSIGNED_LONG:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_LONGLONG;
+                columnFlags = columnFlags | MySQLFieldsType.UNSIGNED_FLAG;
+                break;
+            case DOUBLE:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_DOUBLE;
+                break;
+            case DECIMAL:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_DECIMAL;
+                decimal = true;
+                break;
+            case DATE:
+                columnFlags = MySQLFieldsType.FIELD_TYPE_DATE;
+                break;
+            case DATETIME:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_DATETIME;
+                columnFlags |= MySQLFieldsType.TIMESTAMP_FLAG;
+                break;
+            case TIME:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_TIME;
+                break;
+            case YEAR:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_YEAR;
+                break;
+            case CHAR_BINARY:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_STRING;
+                columnCharsetSet = CharsetMapping.MYSQL_COLLATION_INDEX_binary;
+                break;
+            case CHAR:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_STRING;
+                break;
+            case VARCHAR_BINARY:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_VAR_STRING;
+                columnCharsetSet = CharsetMapping.MYSQL_COLLATION_INDEX_binary;
+                break;
+            case VARCHAR:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_VARCHAR;
+                break;
+            case BINARY:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_BLOB;
+                columnCharsetSet = CharsetMapping.MYSQL_COLLATION_INDEX_binary;
+                columnFlags |= MySQLFieldsType.BLOB_FLAG;
+                columnFlags |= MySQLFieldsType.BINARY_FLAG;
+                break;
+            case FLOAT:
+                mysqlColumnType = MySQLFieldsType.FIELD_TYPE_FLOAT;
+                break;
+        }
+        ColumnDefPacketImpl columnDefPacket = new ColumnDefPacketImpl();
+        byte[] nameBytes = name.getBytes();
+        columnDefPacket.setColumnName(nameBytes);
+        columnDefPacket.setColumnOrgName(nameBytes);
+        columnDefPacket.setColumnType(mysqlColumnType);
+        columnDefPacket.setColumnFlags(columnFlags);
+        columnDefPacket.setColumnLength(columnLength);
+        columnDefPacket.setColumnDecimals(decimal?(byte) 1:0);
+        columnDefPacket.setColumnCharsetSet(columnCharsetSet);
+        return columnDefPacket;
     }
 }
