1d91de99b75feb38b4444d9fb1d57cde7c1ce654
junwen12221 <jamie12221@outlook.com>
2021-12-01 14:30:02 +0800

    improve ViewTest

--- example/src/test/java/io/mycat/view/ViewTest.java
+++ example/src/test/java/io/mycat/view/ViewTest.java
@@ -17,6 +17,11 @@ public class ViewTest implements MycatTest {
     public void testCreatePhyView() throws Exception {
         try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);
              Connection db1 = getMySQLConnection(DB1);) {
+            try {
+                execute(db1, "drop view if exists db1.testView");
+            }catch (Throwable ignored){
+
+            }
             execute(mycatConnection, RESET_CONFIG);
             execute(mycatConnection, "create database " + " db1");
             execute(mycatConnection, "create table db1.normal_table(id int)");
@@ -40,13 +45,64 @@ public class ViewTest implements MycatTest {
     }
 
     @Test
-    public void testCreatePDistView() throws Exception {
+    public void testCreatePhyViewWithColumns() throws Exception {
         try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);
              Connection db1 = getMySQLConnection(DB1);) {
+            try {
+                execute(db1, "drop view if exists db1.testView");
+            }catch (Throwable ignored){
+
+            }
+            execute(mycatConnection, RESET_CONFIG);
+            execute(mycatConnection, "create database " + " db1");
+            execute(mycatConnection, "create table db1.normal_table(id int)");
+            execute(mycatConnection, "create view db1.testView (id2) as select id from db1.normal_table;");
+            deleteData(mycatConnection, "db1", "normal_table");
+            List<Map<String, Object>> maps = executeQuery(mycatConnection, "select * from db1.testView ");
+            Assert.assertTrue(maps.isEmpty());
+            deleteData(mycatConnection, "db1", "normal_table");
+            execute(mycatConnection, "insert db1.normal_table (id) VALUES (1)");
+            maps = executeQuery(mycatConnection, "select * from db1.testView ");
+            Assert.assertTrue(!maps.isEmpty());
+            Assert.assertTrue(maps.toString().contains("id2"));
+            Assert.assertTrue(executeQuery(db1, "show tables from db1").toString()
+                    .toLowerCase().contains("testview"));
+            Assert.assertTrue(executeQuery(mycatConnection, "show tables from db1").toString()
+                    .toLowerCase().contains("testview"));
+            execute(mycatConnection,"drop view db1.testview");
+            Assert.assertTrue(!executeQuery(mycatConnection, "show tables from db1").toString()
+                    .toLowerCase().contains("testview"));
+            System.out.println();
+        }
+    }
+
+
+    @Test
+    public void testCreateDistView() throws Exception {
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);) {
             execute(mycatConnection, RESET_CONFIG);
             execute(mycatConnection, "create database " + " db1");
             execute(mycatConnection, "create view db1.testView as select * from `information_schema`.`COLUMNS`;");
             List<Map<String, Object>> maps = executeQuery(mycatConnection, "select * from db1.testView ");
+            Assert.assertTrue(!maps.isEmpty());
+            Assert.assertTrue(executeQuery(mycatConnection, "show tables from db1").toString()
+                    .toLowerCase().contains("testview"));
+            execute(mycatConnection,"drop view db1.testview");
+            Assert.assertTrue(!executeQuery(mycatConnection, "show tables from db1").toString()
+                    .toLowerCase().contains("testview"));
+            System.out.println();
+        }
+    }
+
+
+    @Test
+    public void testCreateDistViewWithColumns() throws Exception {
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);) {
+            execute(mycatConnection, RESET_CONFIG);
+            execute(mycatConnection, "create database " + " db1");
+            execute(mycatConnection, "create view db1.testView (id2) as select ORDINAL_POSITION from `information_schema`.`COLUMNS`;");
+            List<Map<String, Object>> maps = executeQuery(mycatConnection, "select * from db1.testView ");
+            Assert.assertTrue(maps.toString().contains("id2"));
             Assert.assertTrue(!maps.isEmpty());
             Assert.assertTrue(executeQuery(mycatConnection, "show tables from db1").toString()
                     .toLowerCase().contains("testview"));
