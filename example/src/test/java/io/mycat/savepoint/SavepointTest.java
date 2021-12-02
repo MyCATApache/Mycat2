67ec08f9fa03af0c8246fe5cffcde72d85c0915d
junwen12221 <jamie12221@outlook.com>
2021-12-02 21:36:05 +0800

    fix SavepointTest

--- example/src/test/java/io/mycat/savepoint/SavepointTest.java
+++ example/src/test/java/io/mycat/savepoint/SavepointTest.java
@@ -271,6 +271,7 @@ public class SavepointTest implements MycatTest {
             execute(mycatConnection, RESET_CONFIG);
             initCluster(mycatConnection);
             connectionFunction.accept(mycatConnection);
+            JdbcUtils.execute(mycatConnection, "/*+ mycat:readXARecoveryLog{} */;");
             execute(mycatConnection, "CREATE DATABASE db1");
             execute(mycatConnection, "CREATE TABLE db1.`travelrecord` (\n" +
                     "  `id` bigint NOT NULL AUTO_INCREMENT,\n" +
@@ -293,6 +294,7 @@ public class SavepointTest implements MycatTest {
 
         }
         try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);) {
+            JdbcUtils.execute(mycatConnection, "/*+ mycat:readXARecoveryLog{} */;");
             connectionFunction.accept(mycatConnection);
             execute(mycatConnection, "use db1");
             Assert.assertTrue(executeQuery(mycatConnection,
