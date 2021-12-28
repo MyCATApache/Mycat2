945b3a9c8c0a30f4333ca7699554323c39eeda1c
junwen12221 <jamie12221@outlook.com>
2021-12-28 22:28:45 +0800

    set LockService default ddl timeout to 2 hours

--- common/src/main/java/io/mycat/LockService.java
+++ common/src/main/java/io/mycat/LockService.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 public interface LockService {
     default Future<Lock> getLock(String name) {
-        return getLock(name, TimeUnit.DAYS.toMillis(2));
+        return getLock(name, TimeUnit.HOURS.toMillis(2));
     }
 
     Future<Lock> getLock(String name, long timeout);
