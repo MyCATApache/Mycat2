cc715eafbb4e36ed6757bc03ae1a6a64dd6bd4e8
junwen12221 <jamie12221@outlook.com>
2022-02-08 17:35:16 +0800

    fix test

--- example/src/test/java/io/mycat/monitor/MycatMonitorTest.java
+++ example/src/test/java/io/mycat/monitor/MycatMonitorTest.java
@@ -29,7 +29,6 @@ import java.util.Map;
 @net.jcip.annotations.NotThreadSafe
 public class MycatMonitorTest implements MycatTest {
     public static Vertx vertx;
-    boolean init = false;
 
     @BeforeClass
     public static void beforeClass() {
@@ -50,17 +49,12 @@ public class MycatMonitorTest implements MycatTest {
     @Before
     @SneakyThrows
     public synchronized void before() {
-        if (!init) {
-            try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);) {
-                execute(mycatConnection, "CREATE DATABASE db1");
-                execute(mycatConnection, "CREATE TABLE db1.`monitor` (\n" +
-                        "  `id` bigint(20) NOT NULL KEY " +
-                        ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4\n");
-            }
-            init = true;
+        try (Connection mycatConnection = getMySQLConnection(DB_MYCAT);) {
+            execute(mycatConnection, "CREATE DATABASE if not exists db1");
+            execute(mycatConnection, "CREATE TABLE if not exists db1.`monitor` (\n" +
+                    "  `id` bigint(20) NOT NULL KEY " +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4\n");
         }
-
-
     }
 
     @Test
