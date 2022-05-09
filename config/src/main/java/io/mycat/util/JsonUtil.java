b6aff083619cd930f794733a57a36082af218e47
lightfish <jamie12221@outlook.com>
2022-05-09 21:03:41 +0800

    add io.mycat.util.JsonUtil.clone

--- config/src/main/java/io/mycat/util/JsonUtil.java
+++ config/src/main/java/io/mycat/util/JsonUtil.java
@@ -9,21 +9,21 @@ import java.util.Map;
 import java.util.Optional;
 
 public class JsonUtil {
-    public static Map<String, String> urlSplit(String s){
+    public static Map<String, String> urlSplit(String s) {
         Map<String, String> mapRequest = new HashMap<>(2);
         String strUrlParam = truncateUrlPage(s);
-        if(strUrlParam == null){
+        if (strUrlParam == null) {
             return mapRequest;
         }
         String[] arrSplit = strUrlParam.split("[&]");
-        for(String strSplit : arrSplit){
+        for (String strSplit : arrSplit) {
             String[] arrSplitEqual = strSplit.split("[=]");
             //解析出键值
-            if(arrSplitEqual.length>1){
+            if (arrSplitEqual.length > 1) {
                 //正确解析
                 mapRequest.put(arrSplitEqual[0], arrSplitEqual[1]);
-            }else{
-                if(!"".equals(arrSplitEqual[0])){
+            } else {
+                if (!"".equals(arrSplitEqual[0])) {
                     //只有参数没有值，不加入
                     mapRequest.put(arrSplitEqual[0], "");
                 }
@@ -32,18 +32,22 @@ public class JsonUtil {
         return mapRequest;
     }
 
-    private static String truncateUrlPage(String strURL){
-        String strAllParam=null;
+    private static String truncateUrlPage(String strURL) {
+        String strAllParam = null;
         strURL = strURL.trim();
         String[] arrSplit = strURL.split("[?]");
-        if(strURL.length() > 1){
-            if(arrSplit.length > 1){
-                for (int i=1; i < arrSplit.length; i++){
+        if (strURL.length() > 1) {
+            if (arrSplit.length > 1) {
+                for (int i = 1; i < arrSplit.length; i++) {
                     strAllParam = arrSplit[i];
                 }
             }
         }
         return strAllParam;
+    }
+
+    public static <T> T clone(Object o, Class<T> clazz) {
+        return from(toJson(o), clazz);
     }
 
     public static <T> T from(String text, Class<T> clazz) {
